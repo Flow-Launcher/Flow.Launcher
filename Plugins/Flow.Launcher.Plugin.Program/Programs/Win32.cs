f40d4fc782b9f0bb9e72fb68333ead6a210a3f8c
Jeremy Wu <jeremy24wu@gmail.com>
2020-04-25 18:44:02 +1000

    Fix bug loading incorrect class for Lnk extension files

--- Plugins/Flow.Launcher.Plugin.Program/Programs/Win32.cs
+++ Plugins/Flow.Launcher.Plugin.Program/Programs/Win32.cs
@@ -342,7 +342,7 @@ namespace Flow.Launcher.Plugin.Program.Programs
             var paths = listToAdd.Distinct().ToArray();
 
             var programs1 = paths.AsParallel().Where(p => Extension(p) == ExeExtension).Select(ExeProgram);
-            var programs2 = paths.AsParallel().Where(p => Extension(p) == ShortcutExtension).Select(ExeProgram);
+            var programs2 = paths.AsParallel().Where(p => Extension(p) == ShortcutExtension).Select(LnkProgram);
             var programs3 = from p in paths.AsParallel()
                             let e = Extension(p)
                             where e != ShortcutExtension && e != ExeExtension
