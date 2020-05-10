2b7adc9fd42b106801d401df7ac5237861d3d025
Jeremy Wu <jeremy24wu@gmail.com>
2020-05-10 19:49:48 +1000

    Add skeleton tests and classes for Explorer

---
+++ Flow.Launcher.Test/Plugins/ExplorerTest.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace Flow.Launcher.Test.Plugins
+{
+    [TestFixture]
+    public class ExplorerTest
+    {
+        public void WhenGivenFolderPathWindowsIndexSearchQuery_QueryStringShouldUseDirectoryCommand() { }
+
+        public void WhenSearchAllFoldersAndFilesWindowsIndexSearchQuery_QueryStringShouldUseScopeCommand() { }
+
+        public void WhenWindowsIndexSearchReturnedNilAndIsNotIndexed_NewSearchMethodShouldUseDirectoryInfoClass() { }
+
+        public void WhenSearchPatternHotKeyIsSearchAll_WindowsIndexSearchQueryStringShouldUseScopeCommand() { }
+    }
+}
