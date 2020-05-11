6d7aa46747ee730679e07151bfc4ef0c5bd19d01
Jeremy Wu <jeremy24wu@gmail.com>
2020-05-11 23:19:41 +1000

    Add Windows Index directory search query test

--- Flow.Launcher.Test/Plugins/ExplorerTest.cs
+++ Flow.Launcher.Test/Plugins/ExplorerTest.cs
@@ -1,11 +1,28 @@
+using Flow.Launcher.Plugin.Explorer;
+using Flow.Launcher.Plugin.Explorer.Search.WindowsIndex;
 using NUnit.Framework;
+using System;
 
 namespace Flow.Launcher.Test.Plugins
 {
     [TestFixture]
     public class ExplorerTest
     {
-        public void GivenWindowsIndexSearch_WhenProvidedFolderPath_ThenQueryWhereRestrictionsShouldUseDirectoryString(string expectedString){}
+        [TestCase("directory='file:{path}'")]
+        public void GivenWindowsIndexSearch_WhenProvidedFolderPath_ThenQueryWhereRestrictionsShouldUseDirectoryString(string expectedString)
+        {
+            // Given
+            var queryConstructor = new QueryConstructor(new Settings());
+
+            // When
+            var path = @"C:\Dropbox";
+            var result = queryConstructor.QueryWhereRestrictionsForTopLevelDirectorySearch(path);
+
+            // Then
+            Assert.IsTrue(result == expectedString,
+                $"Expected QueryWhereRestrictions string: {expectedString}{Environment.NewLine} " +
+                $"Actual: {result}{Environment.NewLine}");
+        }
 
         public void GivenWindowsIndexSearch_WhenSearchAllFoldersAndFiles_ThenQueryWhereRestrictionsShouldUseScopeString() { }
 
