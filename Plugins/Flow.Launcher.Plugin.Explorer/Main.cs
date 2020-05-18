c295585272d6dca918f26a1bb103d8fc4bea3dd8
Jeremy Wu <jeremy24wu@gmail.com>
2020-05-19 00:14:41 +1000

    Update Main to handle basic files and folder search query

--- Plugins/Flow.Launcher.Plugin.Explorer/Main.cs
+++ Plugins/Flow.Launcher.Plugin.Explorer/Main.cs
@@ -1,14 +1,16 @@
 ﻿using Flow.Launcher.Infrastructure.Storage;
+using Flow.Launcher.Plugin.Explorer.Search.WindowsIndex;
 using Flow.Launcher.Plugin.Explorer.ViewModels;
 using Flow.Launcher.Plugin.Explorer.Views;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Windows.Controls;
 
 namespace Flow.Launcher.Plugin.Explorer
 {
-    class Main : ISettingProvider, IPlugin, ISavable, IPluginI18n, IContextMenu
+    class Main : ISettingProvider, IPlugin, ISavable, IContextMenu //, IPluginI18n <=== do later
     {
         private PluginInitContext Context { get; set; }
 
@@ -16,19 +18,12 @@ namespace Flow.Launcher.Plugin.Explorer
 
         private SettingsViewModel _viewModel;
 
+        // Reserved keywords in oleDB
+        private string ReservedStringPattern = @"^[\/\\\$\%]+$";
+
         public Control CreateSettingPanel()
         {
             return new ExplorerSettings();
-        }
-
-        public string GetTranslatedPluginDescription()
-        {
-            throw new NotImplementedException();
-        }
-
-        public string GetTranslatedPluginTitle()
-        {
-            throw new NotImplementedException();
         }
 
         public void Init(PluginInitContext context)
@@ -41,12 +36,28 @@ namespace Flow.Launcher.Plugin.Explorer
 
         public List<Result> LoadContextMenus(Result selectedResult)
         {
-            throw new NotImplementedException();
+            return new List<Result>();
         }
 
         public List<Result> Query(Query query)
         {
-            return new List<Result>();
+            var results = new List<Result>();
+
+            if (string.IsNullOrEmpty(query.Search))
+                return results;
+
+            var regexMatch = Regex.Match(query.Search, ReservedStringPattern);
+
+            if (regexMatch.Success)
+                return results;
+
+            var queryConstructor = new QueryConstructor(_settings);
+
+            var searcher = new Searcher(Context);
+
+            return searcher.WindowsIndexSearch(query.Search,
+                                               queryConstructor.CreateQueryHelper().ConnectionString,
+                                               queryConstructor.QueryForAllFilesAndFolders);
         }
 
         public void Save()
