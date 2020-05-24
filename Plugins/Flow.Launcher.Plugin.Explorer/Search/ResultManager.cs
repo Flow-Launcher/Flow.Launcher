191efe31cbc24a1753e2d9fef5b3ef420ad57bd3
Jeremy Wu <jeremy24wu@gmail.com>
2020-05-24 19:11:40 +1000

    Add ResultManager for handling creation of folder and file result

---
+++ Plugins/Flow.Launcher.Plugin.Explorer/Search/ResultManager.cs
@@ -0,0 +1,59 @@
+﻿using Flow.Launcher.Infrastructure;
+using Flow.Launcher.Plugin.SharedCommands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace Flow.Launcher.Plugin.Explorer.Search
+{
+    public class ResultManager
+    {
+        public Result CreateFolderResult(string title, string subtitle, string path, Query query)
+        {
+            return new Result
+            {
+                Title = title,
+                IcoPath = path,
+                SubTitle = subtitle,
+                TitleHighlightData = StringMatcher.FuzzySearch(query.Search, title).MatchData,
+                Action = c =>
+                {
+                    if (c.SpecialKeyState.CtrlPressed)
+                    {
+                        try
+                        {
+                            FilesFolders.OpenPath(path);
+                            return true;
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message, "Could not start " + path);
+                            return false;
+                        }
+                    }
+                    
+                    string changeTo = path.EndsWith("\\") ? path : path + "\\";
+                    Main.Context.API.ChangeQuery(string.IsNullOrEmpty(query.ActionKeyword) ?
+                        changeTo :
+                        query.ActionKeyword + " " + changeTo);
+                    return false;
+                },
+                ContextData = new SearchResult { Type = ResultType.Folder, FullPath = path }
+            };
+        }
+    }
+
+    public class SearchResult
+    {
+        public string FullPath { get; set; }
+        public ResultType Type { get; set; }
+    }
+
+    public enum ResultType
+    {
+        Volume,
+        Folder,
+        File
+    }
+}
