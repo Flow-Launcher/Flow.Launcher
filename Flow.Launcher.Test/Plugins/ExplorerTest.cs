0a559f828565e6f93a91cb9abe334562d2649bec
Jeremy Wu <jeremy24wu@gmail.com>
2020-06-11 08:09:49 +1000

    remove tests not using query constructor

--- Flow.Launcher.Test/Plugins/ExplorerTest.cs
+++ Flow.Launcher.Test/Plugins/ExplorerTest.cs
@@ -140,103 +140,6 @@ namespace Flow.Launcher.Test.Plugins
                 $"Actual string was: {resultString}{Environment.NewLine}");
         }
 
-        [TestCase]
-        public void GivenTopLevelDirectorySearch_WhenIndexSearchNotRequired_ThenSearchMethodShouldContinueDirectoryInfoClassSearch() 
-        {
-            // Given
-            var searchManager = new SearchManager(new Settings(), new PluginInitContext());
-            
-            // When
-            var results = searchManager.TopLevelDirectorySearchBehaviour(
-                                            MethodWindowsIndexSearchReturnsZeroResults, 
-                                            MethodDirectoryInfoClassSearchReturnsTwoResults, 
-                                            false, 
-                                            new Query(),
-                                            "string not used");
-
-            // Then
-            Assert.IsTrue(results.Count == 2,
-                $"Expected to have 2 results from DirectoryInfoClassSearch {Environment.NewLine} " +
-                $"Actual number of results is {results.Count} {Environment.NewLine}");
-        }
-
-        [TestCase]
-        public void GivenTopLevelDirectorySearch_WhenIndexSearchNotRequired_ThenSearchMethodShouldNotContinueDirectoryInfoClassSearch()
-        {
-            // Given
-            var searchManager = new SearchManager(new Settings(), new PluginInitContext());
-
-            // When
-            var results = searchManager.TopLevelDirectorySearchBehaviour(
-                                            MethodWindowsIndexSearchReturnsZeroResults,
-                                            MethodDirectoryInfoClassSearchReturnsTwoResults,
-                                            true,
-                                            new Query(),
-                                            "string not used");
-
-            // Then
-            Assert.IsTrue(results.Count == 0,
-                $"Expected to have 0 results because location is indexed {Environment.NewLine} " +
-                $"Actual number of results is {results.Count} {Environment.NewLine}");
-        }
-
-        [TestCase(@"c:\\", false)]
-        [TestCase(@"i:\", true)]
-        [TestCase(@"\c:\", false)]
-        [TestCase(@"cc:\", false)]
-        [TestCase(@"\\\SomeNetworkLocation\", false)]
-        [TestCase("RandomFile", false)]
-        public void WhenGivenQuerySearchString_ThenShouldIndicateIfIsLocationPathString(string querySearchString, bool expectedResult)
-        {
-            // When, Given
-            var result = FilesFolders.IsLocationPathString(querySearchString);
-
-            //Then
-            Assert.IsTrue(result == expectedResult,
-                $"Expected query search string check result is: {expectedResult} {Environment.NewLine} " +
-                $"Actual check result is {result} {Environment.NewLine}");
-
-        }
-        
-        [TestCase(@"C:\SomeFolder\SomeApp", true, @"C:\SomeFolder\")]
-        [TestCase(@"C:\SomeFolder\SomeApp\SomeFile", true, @"C:\SomeFolder\SomeApp\")]
-        [TestCase(@"C:\NonExistentFolder\SomeApp", false, "")]
-        public void GivenAPartialPath_WhenPreviousLevelDirectoryExists_ThenShouldReturnThePreviousDirectoryPathString(
-            string path, bool previousDirectoryExists, string expectedString)
-        {
-            // When
-            Func<string, bool> previousLocationExists = null;
-            if (previousDirectoryExists)
-            {
-                previousLocationExists = PreviousLocationExistsReturnsTrue;
-            }
-            else
-            {
-                previousLocationExists = PreviousLocationNotExistReturnsFalse;
-            }
-
-            // Given
-            var previousDirectoryPath = FilesFolders.GetPreviousExistingDirectory(previousLocationExists, path);
-
-            //Then
-            Assert.IsTrue(previousDirectoryPath == expectedString,
-                $"Expected path string: {expectedString} {Environment.NewLine} " +
-                $"Actual path string is {previousDirectoryPath} {Environment.NewLine}");
-        }
-
-        [TestCase(@"C:\NonExistentFolder\SomeApp", @"C:\NonExistentFolder\")]
-        [TestCase(@"C:\NonExistentFolder\SomeApp\", @"C:\NonExistentFolder\SomeApp\")]
-        public void WhenGivenAPath_ThenShouldReturnThePreviousDirectoryPathIfIncompleteOrOriginalString(
-            string path, string expectedString)
-        {
-            var returnedPath = FilesFolders.ReturnPreviousDirectoryIfIncompleteString(path);
-
-            //Then
-            Assert.IsTrue(returnedPath == expectedString,
-                $"Expected path string: {expectedString} {Environment.NewLine} " +
-                $"Actual path string is {returnedPath} {Environment.NewLine}");
-        }
-
         [TestCase("c:\\SomeFolder\\>", "scope='file:c:\\SomeFolder'")]
         [TestCase("c:\\SomeFolder\\>SomeName", "(System.FileName LIKE 'SomeName%' " +
                                                         "OR CONTAINS(System.FileName,'\"SomeName*\"',1033)) AND " +
@@ -253,23 +156,6 @@ namespace Flow.Launcher.Test.Plugins
             Assert.IsTrue(resultString == expectedString,
                 $"Expected QueryWhereRestrictions string: {expectedString}{Environment.NewLine} " +
                 $"Actual string was: {resultString}{Environment.NewLine}");
-        }
-
-        [TestCase("c:\\somefolder\\>somefile","*somefile*")]
-        [TestCase("c:\\somefolder\\somefile", "somefile*")]
-        [TestCase("c:\\somefolder\\", "*")]
-        public void GivenDirectoryInfoSearch_WhenSearchPatternHotKeyIsSearchAll_ThenSearchCriteriaShouldUseCriteriaString(string path, string expectedString)
-        {
-            // Given
-            var criteriaConstructor = new DirectoryInfoSearch(new PluginInitContext());
-
-            //When
-            var resultString = criteriaConstructor.ConstructSearchCriteria(path);
-
-            // Then
-            Assert.IsTrue(resultString == expectedString,
-                $"Expected criteria string: {expectedString}{Environment.NewLine} " +
-                $"Actual criteria string was: {resultString}{Environment.NewLine}");
         }
     }
 }
