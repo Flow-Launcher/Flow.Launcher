fc37246a86a8a9ff91710921e1edbf32cb2cd561
Jeremy Wu <jeremy24wu@gmail.com>
2020-06-22 06:16:37 +1000

    settings window- update query time translation on language change

--- Flow.Launcher/ViewModel/PluginViewModel.cs
+++ Flow.Launcher/ViewModel/PluginViewModel.cs
@@ -24,7 +24,7 @@ namespace Flow.Launcher.ViewModel
         }
         public Visibility ActionKeywordsVisibility => PluginPair.Metadata.ActionKeywords.Count > 1 ? Visibility.Collapsed : Visibility.Visible;
         public string InitilizaTime => PluginPair.Metadata.InitTime.ToString() + "ms";
-        public string QueryTime => string.Format(_translator.GetTranslation("plugin_query_time"), PluginPair.Metadata.AvgQueryTime);
+        public string QueryTime => PluginPair.Metadata.AvgQueryTime + "ms";
         public string ActionKeywordsText => string.Join(Query.ActionKeywordSeperater, PluginPair.Metadata.ActionKeywords);
 
         public void ChangeActionKeyword(string newActionKeyword, string oldActionKeyword)
