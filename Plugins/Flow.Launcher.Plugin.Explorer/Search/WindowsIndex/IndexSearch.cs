b44b67220a5f4da227146f9deeb046501f9cbdd1
Jeremy Wu <jeremy24wu@gmail.com>
2020-08-31 07:39:05 +1000

    encode # symbol part of the path when creating uri

--- Plugins/Flow.Launcher.Plugin.Explorer/Search/WindowsIndex/IndexSearch.cs
+++ Plugins/Flow.Launcher.Plugin.Explorer/Search/WindowsIndex/IndexSearch.cs
@@ -51,7 +51,10 @@ namespace Flow.Launcher.Plugin.Explorer.Search.WindowsIndex
                                 {
                                     if (dataReaderResults.GetValue(0) != DBNull.Value && dataReaderResults.GetValue(1) != DBNull.Value)
                                     {
-                                        var path = new Uri(dataReaderResults.GetString(1)).LocalPath;
+                                        // # is URI syntax for the fragment component, need to be encoded so LocalPath returns complete path   
+                                        var encodedFragmentPath = dataReaderResults.GetString(1).Replace("#", "%23");
+                                        
+                                        var path = new Uri(encodedFragmentPath).LocalPath;
 
                                         if (dataReaderResults.GetString(2) == "Directory")
                                         {
