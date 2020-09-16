1e5d7bd928f6e77abbee00f5f9cc0f09fc0309a8
Jeremy Wu <jeremy24wu@gmail.com>
2020-09-16 21:12:43 +1000

    add class to load assembly and resolve dependencies for each plugin

---
+++ Flow.Launcher.Core/Plugin/PluginAssemblyLoader.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Loader;
+
+namespace Flow.Launcher.Core.Plugin
+{
+    internal class PluginAssemblyLoader : AssemblyLoadContext
+    {
+        private readonly AssemblyDependencyResolver dependencyResolver;
+
+        private readonly AssemblyName assemblyName;
+
+        internal PluginAssemblyLoader(string assemblyFilePath)
+        {
+            dependencyResolver = new AssemblyDependencyResolver(assemblyFilePath);
+            assemblyName = new AssemblyName(Path.GetFileNameWithoutExtension(assemblyFilePath));
+        }
+
+        internal Assembly LoadAssemblyAndDependencies()
+        {
+            return LoadFromAssemblyName(assemblyName);
+        }
+
+        protected override Assembly Load(AssemblyName assemblyName)
+        {
+            string assemblyPath = dependencyResolver.ResolveAssemblyToPath(assemblyName);
+
+            if (assemblyPath != null)
+            {
+                return LoadFromAssemblyPath(assemblyPath);
+            }
+
+            return null;
+        }
+
+        internal Type FromAssemblyGetTypeOfInterface(Assembly assembly, Type type)
+        {
+            var allTypes = assembly.ExportedTypes;
+
+            return allTypes.First(o => o.IsClass && !o.IsAbstract && o.GetInterfaces().Contains(type));
+        }
+    }
+}
