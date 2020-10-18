e264af500fa129e74e6f22c6a2a283bf7d11560c
弘韬 张 <hongtao_zhang@outlook.com>
2020-10-18 21:24:59 +0800

    merge one extra condition to the switch case

--- Flow.Launcher.Infrastructure/StringMatcher.cs
+++ Flow.Launcher.Infrastructure/StringMatcher.cs
@@ -76,16 +76,11 @@ namespace Flow.Launcher.Infrastructure
                 if (currentQueryIndex >= queryWithoutCase.Length)
                     break;
 
-                if (compareIndex == 0 && queryWithoutCase[currentQueryIndex] == char.ToLower(stringToCompare[compareIndex]))
-                {
-                    acronymMatchData.Add(compareIndex);
-                    currentQueryIndex++;
-                    continue;
-                }
 
                 switch (stringToCompare[compareIndex])
                 {
-                    case char c when (char.IsUpper(c) && char.ToLower(c) == queryWithoutCase[currentQueryIndex])
+                    case char c when compareIndex == 0 && queryWithoutCase[currentQueryIndex] == char.ToLower(stringToCompare[compareIndex])
+                                  || (char.IsUpper(c) && char.ToLower(c) == queryWithoutCase[currentQueryIndex])
                                   || (char.IsWhiteSpace(c) && char.ToLower(stringToCompare[++compareIndex]) == queryWithoutCase[currentQueryIndex]):
                         acronymMatchData.Add(compareIndex);
                         currentQueryIndex++;
