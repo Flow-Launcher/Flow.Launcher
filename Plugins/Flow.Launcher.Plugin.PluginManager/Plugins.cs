9ae9d69a570fa2073979b9dbeb31dc23112c5d07
Jeremy Wu <jeremy24wu@gmail.com>
2020-12-06 20:39:26 +1100

    update Plugin json mapping

--- Plugins/Flow.Launcher.Plugin.PluginManager/Plugins.cs
+++ Plugins/Flow.Launcher.Plugin.PluginManager/Plugins.cs
@@ -6,8 +6,14 @@ namespace Flow.Launcher.Plugin.PluginsManager
 {
     internal class Plugin
     {
-        public string Name { get; set; }
-        public string Url { get; set; }
-        public string Version { get; set; }
+        internal string ID { get; set; }
+        internal string Name { get; set; }
+        internal string Description { get; set; }
+        internal string Author { get; set; }
+        internal string Version { get; set; }
+        internal string Language { get; set; }
+        internal string Website { get; set; }
+        internal string UrlDownload { get; set; }
+        internal string UrlSourceCode { get; set; }
     }
 }
