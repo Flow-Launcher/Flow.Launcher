c8b251d63ac0bdac58ba2fc1221566d88e4488e9
Jeremy Wu <jeremy24wu@gmail.com>
2020-12-14 08:41:18 +1100

    use Http.Get from Infrastructure

--- Plugins/Flow.Launcher.Plugin.PluginsManager/Models/PluginsManifest.cs
+++ Plugins/Flow.Launcher.Plugin.PluginsManager/Models/PluginsManifest.cs
@@ -3,8 +3,7 @@ using Flow.Launcher.Infrastructure.Logger;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
-using System.Net;
-using System.Text;
+using System.Threading.Tasks;
 
 namespace Flow.Launcher.Plugin.PluginsManager.Models
 {
@@ -13,23 +12,29 @@ namespace Flow.Launcher.Plugin.PluginsManager.Models
         internal List<UserPlugin> UserPlugins { get; private set; }
         internal PluginsManifest()
         {
+            DownloadManifest();
+        }
+
+        private void DownloadManifest()
+        {
             var json = string.Empty;
-
-            using (var wc = new WebClient { Proxy = Http.WebProxy() })
+            try
             {
-                try
-                {
-                    json = wc.DownloadString("https://raw.githubusercontent.com/Flow-Launcher/Flow.Launcher.PluginsManifest/main/plugins.json");
-                }
-                catch (Exception e)
-                {
-                    Log.Exception("|PluginManagement.GetManifest|Encountered error trying to download plugins manifest", e);
+                var t = Task.Run(
+                            async () => 
+                                json = await Http.Get("https://raw.githubusercontent.com/Flow-Launcher/Flow.Launcher.PluginsManifest/main/plugins.json"));
 
-                    UserPlugins = new List<UserPlugin>();
-                }
+                t.Wait();
+
+                UserPlugins = JsonConvert.DeserializeObject<List<UserPlugin>>(json);
+            }
+            catch (Exception e)
+            {
+                Log.Exception("|PluginManagement.GetManifest|Encountered error trying to download plugins manifest", e);
+
+                UserPlugins = new List<UserPlugin>();
             }
 
-            UserPlugins = !string.IsNullOrEmpty(json) ? JsonConvert.DeserializeObject<List<UserPlugin>>(json) : new List<UserPlugin>();
         }
     }
 }
