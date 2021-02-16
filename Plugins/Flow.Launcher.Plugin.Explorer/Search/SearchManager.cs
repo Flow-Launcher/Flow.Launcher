b2f841ebb958d8f49aa35a4ac43222e79e0eec6c
Jeremy Wu <jeremy24wu@gmail.com>
2021-02-17 08:50:33 +1100

    remove duplicate entry with quick access results

--- Plugins/Flow.Launcher.Plugin.Explorer/Search/SearchManager.cs
+++ Plugins/Flow.Launcher.Plugin.Explorer/Search/SearchManager.cs
@@ -38,7 +38,7 @@ namespace Flow.Launcher.Plugin.Explorer.Search
             var quickaccessLinks = QuickAccess.AccessLinkListMatched(query, settings.QuickAccessLinks);
 
             if (quickaccessLinks.Count > 0)
-                results.AddRange(quickaccessLinks);
+                results.Union(quickaccessLinks);
 
             var isEnvironmentVariable = EnvironmentVariables.IsEnvironmentVariableSearch(querySearch);
 
