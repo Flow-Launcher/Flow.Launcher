4a3ad0854868f87e30401cf4bda986457db5bf3a
弘韬 张 <hongtao_zhang@outlook.com>
2021-02-17 22:36:35 +0800

    Use IReadOnlyCollection to avoid avoid duplicate iteration Add distinct to NewResults to try avoiding duplicate result

--- Flow.Launcher/ViewModel/ResultsViewModel.cs
+++ Flow.Launcher/ViewModel/ResultsViewModel.cs
@@ -1,16 +1,13 @@
-﻿using System;
+﻿using Flow.Launcher.Infrastructure.UserSettings;
+using Flow.Launcher.Plugin;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.Linq;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
-using Flow.Launcher.Infrastructure.UserSettings;
-using Flow.Launcher.Plugin;
 
 namespace Flow.Launcher.ViewModel
 {
@@ -146,7 +143,7 @@ namespace Flow.Launcher.ViewModel
         /// <summary>
         /// To avoid deadlock, this method should not called from main thread
         /// </summary>
-        public void AddResults(IEnumerable<ResultsForUpdate> resultsForUpdates, CancellationToken token)
+        public void AddResults(IReadOnlyCollection<ResultsForUpdate> resultsForUpdates, CancellationToken token)
         {
             var newResults = NewResults(resultsForUpdates);
 
@@ -191,10 +188,11 @@ namespace Flow.Launcher.ViewModel
             return Results.Where(r => r.Result.PluginID != resultId)
                 .Concat(newResults)
                 .OrderByDescending(r => r.Result.Score)
+                .Distinct()
                 .ToList();
         }
 
-        private List<ResultViewModel> NewResults(IEnumerable<ResultsForUpdate> resultsForUpdates)
+        private List<ResultViewModel> NewResults(IReadOnlyCollection<ResultsForUpdate> resultsForUpdates)
         {
             if (!resultsForUpdates.Any())
                 return Results;
@@ -202,6 +200,7 @@ namespace Flow.Launcher.ViewModel
             return Results.Where(r => r != null && !resultsForUpdates.Any(u => u.ID == r.Result.PluginID))
                           .Concat(resultsForUpdates.SelectMany(u => u.Results, (u, r) => new ResultViewModel(r, _settings)))
                           .OrderByDescending(rv => rv.Result.Score)
+                          .Distinct()
                           .ToList();
         }
         #endregion
