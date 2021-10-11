44c8be12cf372dc59d51a5c509f17a30f6ecd1f0
Dobin Park <onesound@oooz.net>
2021-10-12 00:52:23 +0900

    - Add notification.Wpf

--- Flow.Launcher/Msg.xaml.cs
+++ Flow.Launcher/Msg.xaml.cs
@@ -5,59 +5,28 @@ using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
+using System.Windows.Media;
 using Flow.Launcher.Helper;
 using Flow.Launcher.Infrastructure;
 using Flow.Launcher.Infrastructure.Image;
+using Notification.Wpf;
+using Notification.Wpf.Converters;
 
 namespace Flow.Launcher
 {
     public partial class Msg : Window
     {
-        Storyboard fadeOutStoryboard = new Storyboard();
-        private bool closing;
 
         public Msg()
         {
             InitializeComponent();
-            var screen = Screen.FromPoint(System.Windows.Forms.Cursor.Position);
-            var dipWorkingArea = WindowsInteropHelper.TransformPixelsToDIP(this,
-                screen.WorkingArea.Width,
-                screen.WorkingArea.Height);
-            Left = dipWorkingArea.X - Width;
-            Top = dipWorkingArea.Y;
-            showAnimation.From = dipWorkingArea.Y;
-            showAnimation.To = dipWorkingArea.Y - Height;
 
-            // Create the fade out storyboard
-            fadeOutStoryboard.Completed += fadeOutStoryboard_Completed;
-            DoubleAnimation fadeOutAnimation = new DoubleAnimation(dipWorkingArea.Y - Height, dipWorkingArea.Y, new Duration(TimeSpan.FromSeconds(5)))
-            {
-                AccelerationRatio = 0.2
-            };
-            Storyboard.SetTarget(fadeOutAnimation, this);
-            Storyboard.SetTargetProperty(fadeOutAnimation, new PropertyPath(TopProperty));
-            fadeOutStoryboard.Children.Add(fadeOutAnimation);
-
-            imgClose.Source = ImageLoader.Load(Path.Combine(Infrastructure.Constant.ProgramDirectory, "Images\\close.png"));
-            imgClose.MouseUp += imgClose_MouseUp;
-        }
-
-        void imgClose_MouseUp(object sender, MouseButtonEventArgs e)
-        {
-            if (!closing)
-            {
-                closing = true;
-                fadeOutStoryboard.Begin();
-            }
-        }
-
-        private void fadeOutStoryboard_Completed(object sender, EventArgs e)
-        {
-            Close();
         }
 
         public void Show(string title, string subTitle, string iconPath)
         {
+            var notificationManager = new NotificationManager();
+
             tbTitle.Text = title;
             tbSubTitle.Text = subTitle;
             if (string.IsNullOrEmpty(subTitle))
@@ -72,16 +41,20 @@ namespace Flow.Launcher
                 imgIco.Source = ImageLoader.Load(iconPath);
             }
 
-            Show();
+            var content = new NotificationContent
+            {
+                Title = title,
+                Message = subTitle,
+                Type = NotificationType.Notification,
+                CloseOnClick = true, // Set true if u want close message when left mouse button click on message (base = true)
 
-            Dispatcher.InvokeAsync(async () =>
-                                   {
-                                       if (!closing)
-                                       {
-                                           closing = true;
-                                           await Dispatcher.InvokeAsync(fadeOutStoryboard.Begin);
-                                       }
-                                   });
+                Background = new SolidColorBrush(Colors.Black),
+                Foreground = new SolidColorBrush(Colors.White),
+                Icon = imgIco.Source
+            };
+           
+            notificationManager.Show(content);
         }
+
     }
 }
