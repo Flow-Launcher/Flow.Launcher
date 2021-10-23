fe90ebc7a19018a258c09fd74b1da292b60f14ee
Kevin Zhang <hongtao_zhang@outlook.com>
2021-10-22 23:06:26 -0500

    Use binding to control MinWidth and MaxWidth

--- Flow.Launcher/MainWindow.xaml.cs
+++ Flow.Launcher/MainWindow.xaml.cs
@@ -132,10 +132,6 @@ namespace Flow.Launcher
                             _viewModel.QueryTextCursorMovedToEnd = false;
                         }
                         break;
-                    case nameof(MainViewModel.MainWindowWidth):
-                        MinWidth = _viewModel.MainWindowWidth;
-                        MaxWidth = _viewModel.MainWindowWidth;
-                        break;
                 }
             };
             _settings.PropertyChanged += (o, e) =>
