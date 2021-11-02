4eb36c8ba3a9f14b4e092da4744769ed3ae0de26
Kevin Zhang <hongtao_zhang@outlook.com>
2021-11-02 15:07:11 -0500

    Custom Explorer Binding (Part 1)

---
+++ Flow.Launcher.Infrastructure/UserSettings/CustomExplorerViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flow.Launcher.ViewModel
+{
+    public class CustomExplorerViewModel
+    {
+        public string Name { get; set; }
+        public string Path { get; set; }
+        public string FileArgument { get; set; } = "\"%d\"";
+        public string DirectoryArgument { get; set; } = "\"%d\"";
+        public bool Editable { get; init; } = true;
+    }
+}
