f42a96ebd74fafe122531593dac77209e8930797
Jeremy <jeremy24wu@gmail.com>
2021-11-30 09:07:52 +1100

    update variable name

--- Flow.Launcher.Infrastructure/UserSettings/Settings.cs
+++ Flow.Launcher.Infrastructure/UserSettings/Settings.cs
@@ -15,7 +15,7 @@ namespace Flow.Launcher.Infrastructure.UserSettings
         private string language = "en";
         public string Hotkey { get; set; } = $"{KeyConstant.Alt} + {KeyConstant.Space}";
         public string OpenResultModifiers { get; set; } = KeyConstant.Alt;
-        public string DarkMode { get; set; } = "System";
+        public string ColorScheme { get; set; } = "System";
         public bool ShowOpenResultHotkey { get; set; } = true;
         public double WindowSize { get; set; } = 580;
 
@@ -167,7 +167,7 @@ namespace Flow.Launcher.Infrastructure.UserSettings
         Preserved
     }
 
-    public enum DarkMode
+    public enum ColorSchemes
     {
         System,
         Light,
