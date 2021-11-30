65fe8917f1a2d540a4d309a4ef0427c0d51844b7
DB p <onesound@oooz.net>
2021-12-01 00:44:02 +0900

    Add Page Navigation

--- Flow.Launcher/WelcomeWindow.xaml.cs
+++ Flow.Launcher/WelcomeWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using ModernWpf.Media.Animation;
 using Page = ModernWpf.Controls.Page;
+using WelcomePages = Flow.Launcher.Resources.Pages;
 
 namespace Flow.Launcher
 {
@@ -24,34 +25,89 @@ namespace Flow.Launcher
         public WelcomeWindow()
         {
             InitializeComponent();
-            ContentFrame.Navigate(typeof(SamplePages.SamplePage1));
+            //ContentFrame.Navigate(typeof(SamplePages.SamplePage1));
+            ContentFrame.Navigate(typeof(WelcomePages.WelcomePage1));
+            BackButton.IsEnabled = false;
         }
+        private int page;
+        private int MaxPage = 5;
+        
+        /*public int Page
+        {
+            get { return this.page; }
+            set
+            {
+                this.page = value;
+                if (this.page == 1)
+                {
+                    this.BackButton.IsEnabled = false; 
+                }
+                else 
+                {
+                    this.BackButton.IsEnabled = true;
+                };
+            }
+        }
+        */
 
+        private void ButtonDisabler()
+        {
+            if (page == 0)
+            {
+                BackButton.IsEnabled = false;
+                NextButton.IsEnabled = true;
+            }
+            else if (page == MaxPage - 1)
+            {
+                BackButton.IsEnabled = true;
+                NextButton.IsEnabled = false;
+            }
+            else 
+            {
+                BackButton.IsEnabled = true;
+                NextButton.IsEnabled = true;
+            }
+        }
+        private NavigationTransitionInfo _transitionInfo = new SlideNavigationTransitionInfo() { Effect = SlideNavigationTransitionEffect.FromRight };
+
+        private static Type PageSelector(int a)
+        {
+            switch (a)
+            {
+                case 0:
+                    return typeof(WelcomePages.WelcomePage1);
+                case 1:
+                    return typeof(WelcomePages.WelcomePage2);
+                case 2:
+                    return typeof(WelcomePages.WelcomePage3);
+                case 3:
+                    return typeof(WelcomePages.WelcomePage4);
+                case 4:
+                    return typeof(WelcomePages.WelcomePage5);
+                default:
+                    return typeof(WelcomePages.WelcomePage1);
+            }
+        }
         private void ForwardButton_Click(object sender, RoutedEventArgs e)
         {
-            /*
-            var pageToNavigateTo = ContentFrame.BackStackDepth % 2 == 1 ? typeof(SamplePages.SamplePage1) : typeof(SamplePages.SamplePage2);
-
-            if (_transitionInfo == null)
-            {
-                // Default behavior, no transition set or used.
-                ContentFrame.Navigate(pageToNavigateTo, null);
-            }
-            else
-            {
-                // Explicit transition info used.
+            page = page + 1;
+            ButtonDisabler();
+            var pageToNavigateTo = PageSelector(page);
                 ContentFrame.Navigate(pageToNavigateTo, null, _transitionInfo);
-            }
-            */
         }
 
         private void BackwardButton_Click(object sender, RoutedEventArgs e)
-        {/*
-            if (ContentFrame.BackStackDepth > 0)
+        {
+            if (page > 0)
             {
+                page = page - 1;
+                ButtonDisabler();
                 ContentFrame.GoBack();
             }
-            */
+            else
+            {
+                BackButton.IsEnabled = false;
+            }
         }
         private void BtnCancel_OnClick(object sender, RoutedEventArgs e)
         {
@@ -59,3 +115,4 @@ namespace Flow.Launcher
         }
     }
 }
+
