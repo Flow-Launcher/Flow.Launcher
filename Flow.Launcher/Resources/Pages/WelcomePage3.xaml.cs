fb60c5bd745751d1f5771030049dc8f6a795f01e
DB p <onesound@oooz.net>
2021-12-01 10:27:46 +0900

    Change page image Add Hotkey List Add Recommend queris

--- Flow.Launcher/Resources/Pages/WelcomePage3.xaml.cs
+++ Flow.Launcher/Resources/Pages/WelcomePage3.xaml.cs
@@ -16,57 +16,15 @@ using System.Windows.Shapes;
 
 namespace Flow.Launcher.Resources.Pages
 {
+
     public partial class WelcomePage3 : Page
     {
         public WelcomePage3()
         {
             InitializeComponent();
-        }
-       
-        public string strHotkey { get; set; }
-        public string strHotkeyAction { get; set; }
-        private List<WelcomePage3> HotkeyListData()
-        {
-            List<WelcomePage3> list = new List<WelcomePage3>();
-
-            list.Add(new WelcomePage3()
-            {
-                strHotkey = string.Format(InternationalizationManager.Instance.GetTranslation("Hotkey01")),
-                strHotkeyAction = string.Format(InternationalizationManager.Instance.GetTranslation("Hotkey01Action"))
-            });
-            list.Add(new WelcomePage3()
-            {
-                strHotkey = string.Format(InternationalizationManager.Instance.GetTranslation("Hotkey02")),
-                strHotkeyAction = string.Format(InternationalizationManager.Instance.GetTranslation("Hotkey02Action"))
-            });
-            list.Add(new WelcomePage3()
-            {
-                strHotkey = string.Format(InternationalizationManager.Instance.GetTranslation("Hotkey03")),
-                strHotkeyAction = string.Format(InternationalizationManager.Instance.GetTranslation("Hotkey03Action"))
-            });
-            list.Add(new WelcomePage3()
-            {
-                strHotkey = string.Format(InternationalizationManager.Instance.GetTranslation("Hotkey04")),
-                strHotkeyAction = string.Format(InternationalizationManager.Instance.GetTranslation("Hotkey04Action"))
-            });
-            list.Add(new WelcomePage3()
-            {
-                strHotkey = string.Format(InternationalizationManager.Instance.GetTranslation("Hotkey05")),
-                strHotkeyAction = string.Format(InternationalizationManager.Instance.GetTranslation("Hotkey05Action"))
-            });
-            list.Add(new WelcomePage3()
-            {
-                strHotkey = string.Format(InternationalizationManager.Instance.GetTranslation("Hotkey07")),
-                strHotkeyAction = string.Format(InternationalizationManager.Instance.GetTranslation("Hotkey07Action"))
-            });
-            list.Add(new WelcomePage3()
-            {
-                strHotkey = string.Format(InternationalizationManager.Instance.GetTranslation("Hotkey08")),
-                strHotkeyAction = string.Format(InternationalizationManager.Instance.GetTranslation("Hotkey08Action"))
-            });
-
-            return list;
+            
         }
 
+        
     }
 }
