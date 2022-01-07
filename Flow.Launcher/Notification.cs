66d5772760e0a33e43fcc84d0ceb90c68da2968c
Hongtao Zhang <hongtao_zhang@outlook.com>
2022-01-06 19:15:50 -0600

    Use Microsoft Package to register toast notification

--- Flow.Launcher/Notification.cs
+++ Flow.Launcher/Notification.cs
@@ -1,4 +1,5 @@
 ﻿using Flow.Launcher.Infrastructure;
+using Microsoft.Toolkit.Uwp.Notifications;
 using System;
 using System.IO;
 using Windows.Data.Xml.Dom;
@@ -8,10 +9,17 @@ namespace Flow.Launcher
 {
     internal static class Notification
     {
+        internal static bool legacy = Environment.OSVersion.Version.Build < 19041;
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "<Pending>")]
+        internal static void Uninstall()
+        {
+            if (!legacy)
+                ToastNotificationManagerCompat.Uninstall();
+        }
+
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "<Pending>")]
         public static void Show(string title, string subTitle, string iconPath)
         {
-            var legacy = Environment.OSVersion.Version.Build < 19041;
             // Handle notification for win7/8/early win10
             if (legacy)
             {
@@ -24,13 +32,11 @@ namespace Flow.Launcher
                 ? Path.Combine(Constant.ProgramDirectory, "Images\\app.png")
                 : iconPath;
 
-            var xml = $"<?xml version=\"1.0\"?><toast><visual><binding template=\"ToastImageAndText04\"><image id=\"1\" src=\"{Icon}\" alt=\"meziantou\"/><text id=\"1\">{title}</text>" +
-                      $"<text id=\"2\">{subTitle}</text></binding></visual></toast>";
-            var toastXml = new XmlDocument();
-            toastXml.LoadXml(xml);
-            var toast = new ToastNotification(toastXml);
-            ToastNotificationManager.CreateToastNotifier("Flow Launcher").Show(toast);
-
+            new ToastContentBuilder()
+                .AddText(title, hintMaxLines: 1)
+                .AddText(subTitle)
+                .AddAppLogoOverride(new Uri(Icon))
+                .Show();
         }
 
         private static void LegacyShow(string title, string subTitle, string iconPath)
