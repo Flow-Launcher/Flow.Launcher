8767eb83373055d5c868898e134bd96fdf48bee1
Hongtao Zhang <hongtao_zhang@outlook.com>
2022-02-25 14:23:40 -0600

    Fix Shared Assembly Issue

--- Flow.Launcher.Core/Plugin/PluginAssemblyLoader.cs
+++ Flow.Launcher.Core/Plugin/PluginAssemblyLoader.cs
@@ -11,57 +11,37 @@ namespace Flow.Launcher.Core.Plugin
 {
     internal class PluginAssemblyLoader : AssemblyLoadContext
     {
-        private readonly AssemblyDependencyResolver dependencyResolver;
+        private readonly AssemblyDependencyResolver _dependencyResolver;
 
-        private readonly AssemblyName assemblyName;
-
-        private static readonly ConcurrentDictionary<string, byte> loadedAssembly;
-
-        static PluginAssemblyLoader()
-        {
-            var currentAssemblies = AppDomain.CurrentDomain.GetAssemblies();
-            loadedAssembly = new ConcurrentDictionary<string, byte>(
-                currentAssemblies.Select(x => new KeyValuePair<string, byte>(x.FullName, default)));
-
-            AppDomain.CurrentDomain.AssemblyLoad += (sender, args) =>
-            {
-                loadedAssembly[args.LoadedAssembly.FullName] = default;
-            };
-        }
+        private readonly AssemblyName _assemblyName;
 
         internal PluginAssemblyLoader(string assemblyFilePath)
         {
-            dependencyResolver = new AssemblyDependencyResolver(assemblyFilePath);
-            assemblyName = new AssemblyName(Path.GetFileNameWithoutExtension(assemblyFilePath));
+            _dependencyResolver = new AssemblyDependencyResolver(assemblyFilePath);
+            _assemblyName = new AssemblyName(Path.GetFileNameWithoutExtension(assemblyFilePath));
         }
 
         internal Assembly LoadAssemblyAndDependencies()
         {
-            return LoadFromAssemblyName(assemblyName);
+            return LoadFromAssemblyName(_assemblyName);
         }
 
         protected override Assembly Load(AssemblyName assemblyName)
         {
-            string assemblyPath = dependencyResolver.ResolveAssemblyToPath(assemblyName);
+            string assemblyPath = _dependencyResolver.ResolveAssemblyToPath(assemblyName);
 
             // When resolving dependencies, ignore assembly depenedencies that already exits with Flow.Launcher
             // Otherwise duplicate assembly will be loaded and some weird behavior will occur, such as WinRT.Runtime.dll
             // will fail due to loading multiple versions in process, each with their own static instance of registration state
-            if (assemblyPath == null || ExistsInReferencedPackage(assemblyName))
-                return null;
+            var existAssembly = Default.Assemblies.FirstOrDefault(x => x.FullName == assemblyName.FullName);
 
-            return LoadFromAssemblyPath(assemblyPath);
+            return existAssembly ?? (assemblyPath == null ? null : LoadFromAssemblyPath(assemblyPath));
         }
 
         internal Type FromAssemblyGetTypeOfInterface(Assembly assembly, Type type)
         {
             var allTypes = assembly.ExportedTypes;
             return allTypes.First(o => o.IsClass && !o.IsAbstract && o.GetInterfaces().Any(t => t == type));
-        }
-
-        internal bool ExistsInReferencedPackage(AssemblyName assemblyName)
-        {
-            return loadedAssembly.ContainsKey(assemblyName.FullName);
         }
     }
 }
