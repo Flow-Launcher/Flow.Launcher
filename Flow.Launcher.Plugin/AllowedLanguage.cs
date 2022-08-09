03cedcc37e0aa9dc1e8924991e18b18d0a9c2b9c
Oren Nachman <code@nachmore.com>
2022-08-09 16:38:43 -0700

    AllowedLanguage documentation

--- Flow.Launcher.Plugin/AllowedLanguage.cs
+++ Flow.Launcher.Plugin/AllowedLanguage.cs
@@ -1,34 +1,58 @@
 ﻿namespace Flow.Launcher.Plugin
 {
-#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+    /// <summary>
+    /// Allowed plugin languages
+    /// </summary>
     public static class AllowedLanguage
     {
+        /// <summary>
+        /// Python
+        /// </summary>
         public static string Python
         {
             get { return "PYTHON"; }
         }
 
+        /// <summary>
+        /// C#
+        /// </summary>
         public static string CSharp
         {
             get { return "CSHARP"; }
         }
 
+        /// <summary>
+        /// F#
+        /// </summary>
         public static string FSharp
         {
             get { return "FSHARP"; }
         }
 
+        /// <summary>
+        /// Standard .exe
+        /// </summary>
         public static string Executable
         {
             get { return "EXECUTABLE"; }
         }
 
+        /// <summary>
+        /// Determines if this language is a .NET language
+        /// </summary>
+        /// <param name="language"></param>
+        /// <returns></returns>
         public static bool IsDotNet(string language)
         {
             return language.ToUpper() == CSharp
                 || language.ToUpper() == FSharp;
         }
 
+        /// <summary>
+        /// Determines if this language is supported
+        /// </summary>
+        /// <param name="language"></param>
+        /// <returns></returns>
         public static bool IsAllowed(string language)
         {
             return IsDotNet(language)
@@ -36,5 +60,4 @@
                 || language.ToUpper() == Executable.ToUpper();
         }
     }
-#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
 }
