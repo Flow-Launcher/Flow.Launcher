a9a1c21ece02215dc52d7a3e61be8e19f91be964
DB p <onesound@oooz.net>
2022-09-13 15:26:34 +0900

    - remove 'using system.diagnotics' - fix positioning

--- Flow.Launcher/SettingWindow.xaml.cs
+++ Flow.Launcher/SettingWindow.xaml.cs
@@ -12,7 +12,6 @@ using Flow.Launcher.ViewModel;
 using Microsoft.Win32;
 using ModernWpf;
 using System;
-using System.Diagnostics;
 using System.IO;
 using System.Windows;
 using System.Windows.Controls;
@@ -38,9 +37,10 @@ namespace Flow.Launcher
 
         public SettingWindow(IPublicAPI api, SettingWindowViewModel viewModel)
         {
-            InitializeComponent();
             settings = viewModel.Settings;
             DataContext = viewModel;
+            InitializeComponent();
+            InitializePosition();
             this.viewModel = viewModel;
             API = api;
         }
@@ -49,13 +49,13 @@ namespace Flow.Launcher
 
         private void OnLoaded(object sender, RoutedEventArgs e)
         {
-            InitializePosition();
             RefreshMaximizeRestoreButton();
             // Fix (workaround) for the window freezes after lock screen (Win+L)
             // https://stackoverflow.com/questions/4951058/software-rendering-mode-wpf
             HwndSource hwndSource = PresentationSource.FromVisual(this) as HwndSource;
             HwndTarget hwndTarget = hwndSource.CompositionTarget;
             hwndTarget.RenderMode = RenderMode.SoftwareOnly;
+            InitializePosition();
         }
 
         private void OnSelectPythonDirectoryClick(object sender, RoutedEventArgs e)
