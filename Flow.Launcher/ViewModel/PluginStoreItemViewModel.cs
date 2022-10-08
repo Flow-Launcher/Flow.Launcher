e3e0545c9d96f1ca5503a407732443c60d717d3c
DB p <onesound@oooz.net>
2022-10-08 18:27:17 +0900

    - Fix Mode to oneway - Add sources - Hide Updated label when LabelNew

--- Flow.Launcher/ViewModel/PluginStoreItemViewModel.cs
+++ Flow.Launcher/ViewModel/PluginStoreItemViewModel.cs
@@ -16,12 +16,16 @@ namespace Flow.Launcher.ViewModel
 
         public string Name => _plugin.Name;
         public string Description => _plugin.Description;
+        public string IcoPath => _plugin.IcoPath;
         public string Author => _plugin.Author;
+        public string Website => _plugin.Website;
+        public string Language => _plugin.Language;
+        public string UrlDownload => _plugin.UrlDownload;
+        public string UrlSourceCode => _plugin.UrlSourceCode;
         public string Version => _plugin.Version;
-
 
         public bool LabelNew => _plugin.LatestReleaseDate-DateTime.Now < TimeSpan.FromDays(7);
         public bool LabelInstalled => PluginManager.GetPluginForId(_plugin.ID) != null;
-        public bool LabelUpdated => _plugin.DateAdded -DateTime.Now < TimeSpan.FromDays(7);
+        public bool LabelUpdated => _plugin.DateAdded -DateTime.Now < TimeSpan.FromDays(5) && !LabelNew;
     }
 }
