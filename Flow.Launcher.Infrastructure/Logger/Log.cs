12721bc091b152073fa75fedb13e1635747caff6
Vic <10308169+VictoriousRaptor@users.noreply.github.com>
2022-11-03 01:25:12 +0800

    Revert accidental change in Log.cs

--- Flow.Launcher.Infrastructure/Logger/Log.cs
+++ Flow.Launcher.Infrastructure/Logger/Log.cs
@@ -142,7 +142,7 @@ namespace Flow.Launcher.Infrastructure.Logger
         {
             e = e.Demystify();
 #if DEBUG
-            //ExceptionDispatchInfo.Capture(e).Throw();
+            ExceptionDispatchInfo.Capture(e).Throw();
 #else
             if (FormatValid(message))
             {
