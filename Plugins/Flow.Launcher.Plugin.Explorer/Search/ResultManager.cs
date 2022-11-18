d3ebeb281489e38aa47c55b9cc67c092d7757301
DB p <onesound@oooz.net>
2022-11-18 14:13:57 +0900

    Change Constant Tooltips to String

--- Plugins/Flow.Launcher.Plugin.Explorer/Search/ResultManager.cs
+++ Plugins/Flow.Launcher.Plugin.Explorer/Search/ResultManager.cs
@@ -1,4 +1,5 @@
-﻿using Flow.Launcher.Infrastructure;
+﻿using Flow.Launcher.Core.Resource;
+using Flow.Launcher.Infrastructure;
 using Flow.Launcher.Plugin.SharedCommands;
 using System;
 using System.Diagnostics;
@@ -78,7 +79,7 @@ namespace Flow.Launcher.Plugin.Explorer.Search
                     return false;
                 },
                 Score = score,
-                TitleToolTip = Constants.ToolTipOpenDirectory,
+                TitleToolTip = InternationalizationManager.Instance.GetTranslation("plugin_explorer_plugin_ToolTipOpenDirectory"),
                 SubTitleToolTip = path,
                 ContextData = new SearchResult
                 {
@@ -254,7 +255,7 @@ namespace Flow.Launcher.Plugin.Explorer.Search
 
                     return true;
                 },
-                TitleToolTip = Constants.ToolTipOpenContainingFolder,
+                TitleToolTip = InternationalizationManager.Instance.GetTranslation("plugin_explorer_plugin_ToolTipOpenContainingFolder"),
                 SubTitleToolTip = filePath,
                 ContextData = new SearchResult
                 {
