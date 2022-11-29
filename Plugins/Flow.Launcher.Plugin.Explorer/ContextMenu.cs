c820bc139c9df1a500600e77ea08637fe0a3ed5c
DB p <onesound@oooz.net>
2022-11-29 19:50:53 +0900

    Fix Context Menu Image Icon

--- Plugins/Flow.Launcher.Plugin.Explorer/ContextMenu.cs
+++ Plugins/Flow.Launcher.Plugin.Explorer/ContextMenu.cs
@@ -250,7 +250,7 @@ namespace Flow.Launcher.Plugin.Explorer
                             }
 
                             return false;
-                        }
+                        },
                     });
                 }
 
--- Plugins/Flow.Launcher.Plugin.Explorer/Search/Constants.cs
+++ Plugins/Flow.Launcher.Plugin.Explorer/Search/Constants.cs
@@ -17,7 +17,7 @@ namespace Flow.Launcher.Plugin.Explorer.Search
         internal const string IndexingOptionsIconImagePath = "Images\\windowsindexingoptions.png";
         internal const string QuickAccessImagePath = "Images\\quickaccess.png";
         internal const string RemoveQuickAccessImagePath = "Images\\removequickaccess.png";
-        internal const string ShowContextMenuImagePath = "Images\\contextmenu.png";
+        internal const string ShowContextMenuImagePath = "Images\\context_menu.png";
         internal const string EverythingErrorImagePath = "Images\\everything_error.png";
         internal const string IndexSearchWarningImagePath = "Images\\index_error.png";
         internal const string WindowsIndexErrorImagePath = "Images\\index_error2.png";
