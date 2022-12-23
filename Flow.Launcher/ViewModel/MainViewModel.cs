b18f5f4c02a0024a06a4c02cd8b90d2494d2ef45
Vic <10308169+VictoriousRaptor@users.noreply.github.com>
2022-12-23 11:51:34 +0800

    remove unused using

--- Flow.Launcher/ViewModel/MainViewModel.cs
+++ Flow.Launcher/ViewModel/MainViewModel.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Input;
 using Flow.Launcher.Core.Plugin;
 using Flow.Launcher.Core.Resource;
 using Flow.Launcher.Helper;
@@ -24,7 +23,6 @@ using System.IO;
 using System.Collections.Specialized;
 using CommunityToolkit.Mvvm.Input;
 using System.Globalization;
-using System.Windows.Threading;
 
 namespace Flow.Launcher.ViewModel
 {
