b42fc54b806285067dd2c22cf600fd22c174c3b7
Vic <10308169+VictoriousRaptor@users.noreply.github.com>
2023-01-19 21:33:21 +0800

    Add test for PathContains()

---
+++ Flow.Launcher.Test/FilesFoldersTest.cs
@@ -0,0 +1,53 @@
+﻿using Flow.Launcher.Plugin.SharedCommands;
+using NUnit.Framework;
+
+namespace Flow.Launcher.Test
+{
+    [TestFixture]
+    
+    public class FilesFoldersTest
+    {
+        // Testcases from https://stackoverflow.com/a/31941905/20703207
+        // Disk
+        [TestCase(@"c:", @"c:\foo", true)]
+        [TestCase(@"c:\", @"c:\foo", true)]
+        // Slash
+        [TestCase(@"c:\foo\bar\", @"c:\foo\", false)]
+        [TestCase(@"c:\foo\bar", @"c:\foo\", false)]
+        [TestCase(@"c:\foo", @"c:\foo\bar", true)]
+        [TestCase(@"c:\foo\", @"c:\foo\bar", true)]
+        // File
+        [TestCase(@"c:\foo", @"c:\foo\a.txt", true)]
+        [TestCase(@"c:\foo", @"c:/foo/a.txt", true)]
+        [TestCase(@"c:\FOO\a.txt", @"c:\foo", false)]
+        [TestCase(@"c:\foo\a.txt", @"c:\foo\", false)]
+        [TestCase(@"c:\foobar\a.txt", @"c:\foo", false)]
+        [TestCase(@"c:\foobar\a.txt", @"c:\foo\", false)]
+        [TestCase(@"c:\foo\", @"c:\foo.txt", false)]
+        // Prefix
+        [TestCase(@"c:\foo", @"c:\foobar", false)]
+        [TestCase(@"C:\Program", @"C:\Program Files\", false)]
+        [TestCase(@"c:\foobar", @"c:\foo\a.txt", false)]
+        [TestCase(@"c:\foobar\", @"c:\foo\a.txt", false)]
+        // Edge case
+        [TestCase(@"c:\foo", @"c:\foo\..\bar\baz", false)]
+        [TestCase(@"c:\bar", @"c:\foo\..\bar\baz", true)]
+        [TestCase(@"c:\barr", @"c:\foo\..\bar\baz", false)]
+        // Equality
+        [TestCase(@"c:\foo", @"c:\foo", false)]
+        [TestCase(@"c:\foo\", @"c:\foo", false)]
+        [TestCase(@"c:\foo", @"c:\foo\", false)]
+        public void TestPathContains(string parentPath, string path, bool expectedResult)
+        {
+            Assert.AreEqual(expectedResult, FilesFolders.PathContains(parentPath, path));
+        }
+
+        [TestCase(@"c:\foo", @"c:\foo", true)]
+        [TestCase(@"c:\foo\", @"c:\foo", true)]
+        [TestCase(@"c:\foo\", @"c:\foo", true)]
+        public void TestPathContainsWhenEqual(string parentPath, string path, bool expectedResult)
+        {
+            Assert.AreEqual(expectedResult, FilesFolders.PathContains(parentPath, path, true));
+        }
+    }
+}
