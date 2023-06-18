bb5e1d3a1919caf5d22ecbb72b0cc30c3e62ff03
Ioannis G <jtgftw@gmail.com>
2023-06-18 22:53:01 +0300

    more documentation in Plugin package

--- Flow.Launcher.Plugin/ActionContext.cs
+++ Flow.Launcher.Plugin/ActionContext.cs
@@ -2,18 +2,47 @@
 
 namespace Flow.Launcher.Plugin
 {
+    /// <summary>
+    /// Context provided as a parameter when invoking a
+    /// <see cref="Result.Action"/> or <see cref="Result.AsyncAction"/>
+    /// </summary>
     public class ActionContext
     {
+        /// <summary>
+        /// Contains the press state of certain special keys.
+        /// </summary>
         public SpecialKeyState SpecialKeyState { get; set; }
     }
 
+    /// <summary>
+    /// Contains the press state of certain special keys.
+    /// </summary>
     public class SpecialKeyState
     {
+        /// <summary>
+        /// True if the Ctrl key is pressed.
+        /// </summary>
         public bool CtrlPressed { get; set; }
+
+        /// <summary>
+        /// True if the Shift key is pressed.
+        /// </summary>
         public bool ShiftPressed { get; set; }
+
+        /// <summary>
+        /// True if the Alt key is pressed.
+        /// </summary>
         public bool AltPressed { get; set; }
+
+        /// <summary>
+        /// True if the Windows key is pressed.
+        /// </summary>
         public bool WinPressed { get; set; }
 
+        /// <summary>
+        /// Get this object represented as a <see cref="ModifierKeys"/> flag combination.
+        /// </summary>
+        /// <returns></returns>
         public ModifierKeys ToModifierKeys()
         {
             return (CtrlPressed ? ModifierKeys.Control : ModifierKeys.None) |
