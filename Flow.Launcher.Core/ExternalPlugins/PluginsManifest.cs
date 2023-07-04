f7b585803d120c68685c7ac646e90ea3968d43ca
Ioannis G <jtgftw@gmail.com>
2023-07-04 23:44:35 +0300

    rework how we fetch plugins from manifest URLs
    Introduces the concept of a store of community plugins, which is currently
    limited to the official PluginsManifest repository.
    
    Each store can support more than one manifest file URLs. When fetching, all
    URLs are used until one of them succeeds. This fixes issues with geo-blocking
    such as #2195
    
    Plugin stores can be expanded in the future to be user-configurable, see #2178

--- Flow.Launcher.Core/ExternalPlugins/PluginsManifest.cs
+++ Flow.Launcher.Core/ExternalPlugins/PluginsManifest.cs
@@ -1,10 +1,6 @@
-﻿using Flow.Launcher.Infrastructure.Http;
-using Flow.Launcher.Infrastructure.Logger;
+﻿using Flow.Launcher.Infrastructure.Logger;
 using System;
 using System.Collections.Generic;
-using System.Net;
-using System.Net.Http;
-using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -12,13 +8,13 @@ namespace Flow.Launcher.Core.ExternalPlugins
 {
     public static class PluginsManifest
     {
-        private const string manifestFileUrl = "https://cdn.jsdelivr.net/gh/Flow-Launcher/Flow.Launcher.PluginsManifest@plugin_api_v2/plugins.json";
+        private static readonly CommunityPluginStore mainPluginStore =
+            new("https://raw.githubusercontent.com/Flow-Launcher/Flow.Launcher.PluginsManifest/plugin_api_v2/plugins.json",
+                "https://cdn.jsdelivr.net/gh/Flow-Launcher/Flow.Launcher.PluginsManifest@plugin_api_v2/plugins.json");
 
         private static readonly SemaphoreSlim manifestUpdateLock = new(1);
 
-        private static string latestEtag = "";
-
-        public static List<UserPlugin> UserPlugins { get; private set; } = new List<UserPlugin>();
+        public static List<UserPlugin> UserPlugins { get; private set; }
 
         public static async Task UpdateManifestAsync(CancellationToken token = default)
         {
@@ -26,25 +22,9 @@ namespace Flow.Launcher.Core.ExternalPlugins
             {
                 await manifestUpdateLock.WaitAsync(token).ConfigureAwait(false);
 
-                var request = new HttpRequestMessage(HttpMethod.Get, manifestFileUrl);
-                request.Headers.Add("If-None-Match", latestEtag);
+                var results = await mainPluginStore.FetchAsync(token).ConfigureAwait(false);
 
-                using var response = await Http.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, token).ConfigureAwait(false);
-
-                if (response.StatusCode == HttpStatusCode.OK)
-                {
-                    Log.Info($"|PluginsManifest.{nameof(UpdateManifestAsync)}|Fetched plugins from manifest repo");
-
-                    await using var json = await response.Content.ReadAsStreamAsync(token).ConfigureAwait(false);
-
-                    UserPlugins = await JsonSerializer.DeserializeAsync<List<UserPlugin>>(json, cancellationToken: token).ConfigureAwait(false);
-
-                    latestEtag = response.Headers.ETag.Tag;
-                }
-                else if (response.StatusCode != HttpStatusCode.NotModified)
-                {
-                    Log.Warn($"|PluginsManifest.{nameof(UpdateManifestAsync)}|Http response for manifest file was {response.StatusCode}");
-                }
+                UserPlugins = results;
             }
             catch (Exception e)
             {
