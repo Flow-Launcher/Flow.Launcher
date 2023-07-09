Flow-Launcher/Flow.Launcher

commit 088818bb32812f0308cf80c32a00bbb160974542

Move delegate into EventHandler.cs
#2216

Flow.Launcher.Plugin/EventHandler.cs
@@ -33,6 +33,24 @@ namespace Flow.Launcher.Plugin
     public delegate bool FlowLauncherGlobalKeyboardEventHandler(int keyevent, int vkcode, SpecialKeyState state);
 
     /// <summary>
+    /// A delegate for when the visibility is changed
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="args"></param>
+    public delegate void VisibilityChangedEventHandler(object sender, VisibilityChangedEventArgs args);
+    
+    /// <summary>
+    /// The event args for <see cref="VisibilityChangedEventHandler"/>
+    /// </summary>
+    public class VisibilityChangedEventArgs : EventArgs
+    {
+        /// <summary>
+        /// <see langword="true"/> if the main window has become visible
+        /// </summary>
+        public bool IsVisible { get; init; }
+    }
+
+    /// <summary>
     /// Arguments container for the Key Down event
     /// </summary>
     public class FlowLauncherKeyDownEventArgs

Flow.Launcher.Plugin/VisibilityChangedEventHandler.cs
@@ -1,21 +1,0 @@
-﻿using System;
-
-namespace Flow.Launcher.Plugin
-{
-    /// <summary>
-    /// A delegate for when the visibility is changed
-    /// </summary>
-    /// <param name="sender"></param>
-    /// <param name="args"></param>
-    public delegate void VisibilityChangedEventHandler(object sender, VisibilityChangedEventArgs args);
-    /// <summary>
-    /// The event args for <see cref="VisibilityChangedEventHandler"/>
-    /// </summary>
-    public class VisibilityChangedEventArgs : EventArgs
-    {
-        /// <summary>
-        /// <see langword="true"/> if the main window has become visible
-        /// </summary>
-        public bool IsVisible { get; init; }
-    }
-}

commit 4eb20e242f3e415b45ef9b0fdbccbe69d5e81a57

fix build issue introduced in 088818bb

Flow.Launcher.Plugin/EventHandler.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Input;
 
 namespace Flow.Launcher.Plugin