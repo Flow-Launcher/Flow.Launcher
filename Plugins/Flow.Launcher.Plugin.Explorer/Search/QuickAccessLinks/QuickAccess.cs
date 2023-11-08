9f39dfceee916cbb83e9c88675f083af3abc7406
VictoriousRaptor <10308169+VictoriousRaptor@users.noreply.github.com>
2023-11-08 22:12:01 +0800

    Use FuzzySearch to search access links

--- Plugins/Flow.Launcher.Plugin.Explorer/Search/QuickAccessLinks/QuickAccess.cs
+++ Plugins/Flow.Launcher.Plugin.Explorer/Search/QuickAccessLinks/QuickAccess.cs
@@ -13,20 +13,17 @@ namespace Flow.Launcher.Plugin.Explorer.Search.QuickAccessLinks
             if (string.IsNullOrEmpty(query.Search))
                 return new List<Result>();
 
-            string search = query.Search.ToLower();
-
-            var queriedAccessLinks =
-                accessLinks
-                .Where(x => x.Name.Contains(search, StringComparison.OrdinalIgnoreCase) || x.Path.Contains(search, StringComparison.OrdinalIgnoreCase))
+            return accessLinks
+                .Where(x => Main.Context.API.FuzzySearch(query.Search, x.Name).IsSearchPrecisionScoreMet() || Main.Context.API.FuzzySearch(query.Search, x.Path).IsSearchPrecisionScoreMet())
                 .OrderBy(x => x.Type)
-                .ThenBy(x => x.Name);
-
-            return queriedAccessLinks.Select(l => l.Type switch
-            {
-                ResultType.Folder => ResultManager.CreateFolderResult(l.Name, l.Path, l.Path, query, quickAccessResultScore),
-                ResultType.File => ResultManager.CreateFileResult(l.Path, query, quickAccessResultScore),
-                _ => throw new ArgumentOutOfRangeException()
-            }).ToList();
+                .ThenBy(x => x.Name)
+                .Select(l => l.Type switch
+                    {
+                        ResultType.Folder => ResultManager.CreateFolderResult(l.Name, l.Path, l.Path, query, quickAccessResultScore),
+                        ResultType.File => ResultManager.CreateFileResult(l.Path, query, quickAccessResultScore),
+                        _ => throw new ArgumentOutOfRangeException()
+                    })
+                .ToList();
         }
 
         internal static List<Result> AccessLinkListAll(Query query, IEnumerable<AccessLink> accessLinks)
