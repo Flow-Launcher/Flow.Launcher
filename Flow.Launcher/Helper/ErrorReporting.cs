950b4c91d3243f3e4ed61e3c9f81d6073ed7ba4a
Yusyuriv <Yusyuriv@users.noreply.github.com>
2024-04-11 13:20:38 +0600

    Code cleanup: Flow.Launcher/{Converters,Helper}

--- Flow.Launcher/Helper/ErrorReporting.cs
+++ Flow.Launcher/Helper/ErrorReporting.cs
@@ -4,45 +4,52 @@ using NLog;
 using Flow.Launcher.Infrastructure;
 using Flow.Launcher.Infrastructure.Exception;
 
-namespace Flow.Launcher.Helper
+namespace Flow.Launcher.Helper;
+
+public static class ErrorReporting
 {
-    public static class ErrorReporting
+    private static void Report(Exception e)
     {
-        private static void Report(Exception e)
-        {
-            var logger = LogManager.GetLogger("UnHandledException");
-            logger.Fatal(ExceptionFormatter.FormatExcpetion(e));
-            var reportWindow = new ReportWindow(e);
-            reportWindow.Show();
-        }
+        var logger = LogManager.GetLogger("UnHandledException");
+        logger.Fatal(ExceptionFormatter.FormatExcpetion(e));
+        var reportWindow = new ReportWindow(e);
+        reportWindow.Show();
+    }
 
-        public static void UnhandledExceptionHandle(object sender, UnhandledExceptionEventArgs e)
-        {
-            //handle non-ui thread exceptions
-            Report((Exception)e.ExceptionObject);
-        }
+    public static void UnhandledExceptionHandle(object sender, UnhandledExceptionEventArgs e)
+    {
+        //handle non-ui thread exceptions
+        Report((Exception)e.ExceptionObject);
+    }
 
-        public static void DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
-        {
-            //handle ui thread exceptions
-            Report(e.Exception);
-            //prevent application exist, so the user can copy prompted error info
-            e.Handled = true;
-        }
-        
-        public static string RuntimeInfo()
-        {
-            var info = $"\nFlow Launcher version: {Constant.Version}" +
-                       $"\nOS Version: {ExceptionFormatter.GetWindowsFullVersionFromRegistry()}" +
-                       $"\nIntPtr Length: {IntPtr.Size}" +
-                       $"\nx64: {Environment.Is64BitOperatingSystem}";
-            return info;
-        }
+    public static void DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+    {
+        //handle ui thread exceptions
+        Report(e.Exception);
+        //prevent application exist, so the user can copy prompted error info
+        e.Handled = true;
+    }
 
-        public static string DependenciesInfo()
-        {
-            var info = $"\nPython Path: {Constant.PythonPath}\nNode Path: {Constant.NodePath}";
-            return info;
-        }
+    public static string RuntimeInfo()
+    {
+        var info =
+            $"""
+
+             Flow Launcher version: {Constant.Version}
+             OS Version: {ExceptionFormatter.GetWindowsFullVersionFromRegistry()}
+             IntPtr Length: {IntPtr.Size}
+             x64: {Environment.Is64BitOperatingSystem}
+             """;
+        return info;
+    }
+
+    public static string DependenciesInfo()
+    {
+        var info = $"""
+
+                    Python Path: {Constant.PythonPath}
+                    Node Path: {Constant.NodePath}
+                    """;
+        return info;
     }
 }
--- Flow.Launcher/Helper/WindowsInteropHelper.cs
+++ Flow.Launcher/Helper/WindowsInteropHelper.cs
@@ -8,156 +8,152 @@ using System.Windows.Interop;
 using System.Windows.Media;
 using Point = System.Windows.Point;
 
-namespace Flow.Launcher.Helper
+namespace Flow.Launcher.Helper;
+
+public class WindowsInteropHelper
 {
-    public class WindowsInteropHelper
+    private const int GWL_STYLE = -16; //WPF's Message code for Title Bar's Style
+    private const int WS_SYSMENU = 0x80000; //WPF's Message code for System Menu
+    private static IntPtr _hwnd_shell;
+    private static IntPtr _hwnd_desktop;
+
+    //Accessors for shell and desktop handlers
+    //Will set the variables once and then will return them
+    private static IntPtr HWND_SHELL
     {
-        private const int GWL_STYLE = -16; //WPF's Message code for Title Bar's Style 
-        private const int WS_SYSMENU = 0x80000; //WPF's Message code for System Menu
-        private static IntPtr _hwnd_shell;
-        private static IntPtr _hwnd_desktop;
-
-        //Accessors for shell and desktop handlers
-        //Will set the variables once and then will return them
-        private static IntPtr HWND_SHELL
+        get
         {
-            get
-            {
-                return _hwnd_shell != IntPtr.Zero ? _hwnd_shell : _hwnd_shell = GetShellWindow();
-            }
+            return _hwnd_shell != IntPtr.Zero ? _hwnd_shell : _hwnd_shell = GetShellWindow();
         }
-        private static IntPtr HWND_DESKTOP
+    }
+    private static IntPtr HWND_DESKTOP
+    {
+        get
         {
-            get
-            {
-                return _hwnd_desktop != IntPtr.Zero ? _hwnd_desktop : _hwnd_desktop = GetDesktopWindow();
-            }
+            return _hwnd_desktop != IntPtr.Zero ? _hwnd_desktop : _hwnd_desktop = GetDesktopWindow();
         }
+    }
 
-        [DllImport("user32.dll", SetLastError = true)]
-        internal static extern int GetWindowLong(IntPtr hWnd, int nIndex);
-        
-        [DllImport("user32.dll")]
-        internal static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+    [DllImport("user32.dll", SetLastError = true)]
+    internal static extern int GetWindowLong(IntPtr hWnd, int nIndex);
 
-        [DllImport("user32.dll")]
-        internal static extern IntPtr GetForegroundWindow();
+    [DllImport("user32.dll")]
+    internal static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
 
-        [DllImport("user32.dll")]
-        internal static extern IntPtr GetDesktopWindow();
+    [DllImport("user32.dll")]
+    internal static extern IntPtr GetForegroundWindow();
 
-        [DllImport("user32.dll")]
-        internal static extern IntPtr GetShellWindow();
+    [DllImport("user32.dll")]
+    internal static extern IntPtr GetDesktopWindow();
 
-        [DllImport("user32.dll", SetLastError = true)]
-        internal static extern int GetWindowRect(IntPtr hwnd, out RECT rc);
+    [DllImport("user32.dll")]
+    internal static extern IntPtr GetShellWindow();
 
-        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
-        internal static extern int GetClassName(IntPtr hWnd, StringBuilder lpClassName, int nMaxCount);
+    [DllImport("user32.dll", SetLastError = true)]
+    internal static extern int GetWindowRect(IntPtr hwnd, out RECT rc);
 
-        [DllImport("user32.DLL")]
-        public static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string lpszClass, string lpszWindow);
+    [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+    internal static extern int GetClassName(IntPtr hWnd, StringBuilder lpClassName, int nMaxCount);
+
+    [DllImport("user32.DLL")]
+    public static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string lpszClass, string lpszWindow);
 
 
-        const string WINDOW_CLASS_CONSOLE = "ConsoleWindowClass";
-        const string WINDOW_CLASS_WINTAB = "Flip3D";
-        const string WINDOW_CLASS_PROGMAN = "Progman";
-        const string WINDOW_CLASS_WORKERW = "WorkerW";
+    const string WINDOW_CLASS_CONSOLE = "ConsoleWindowClass";
+    const string WINDOW_CLASS_WINTAB = "Flip3D";
+    const string WINDOW_CLASS_PROGMAN = "Progman";
+    const string WINDOW_CLASS_WORKERW = "WorkerW";
 
-        public static bool IsWindowFullscreen()
+    public static bool IsWindowFullscreen()
+    {
+        //get current active window
+        IntPtr hWnd = GetForegroundWindow();
+
+        if (hWnd.Equals(IntPtr.Zero))
         {
-            //get current active window
-            IntPtr hWnd = GetForegroundWindow();
-
-            if (!hWnd.Equals(IntPtr.Zero))
-            {
-                //if current active window is NOT desktop or shell
-                if (!(hWnd.Equals(HWND_DESKTOP) || hWnd.Equals(HWND_SHELL)))
-                {
-                    StringBuilder sb = new StringBuilder(256);
-                    GetClassName(hWnd, sb, sb.Capacity);
-                    string windowClass = sb.ToString();
-
-                    //for Win+Tab (Flip3D)
-                    if (windowClass == WINDOW_CLASS_WINTAB)
-                    {
-                        return false;
-                    }
-
-                    RECT appBounds;
-                    GetWindowRect(hWnd, out appBounds);
-
-                    //for console (ConsoleWindowClass), we have to check for negative dimensions
-                    if (windowClass == WINDOW_CLASS_CONSOLE)
-                    {
-                        return appBounds.Top < 0 && appBounds.Bottom < 0;
-                    }
-
-                    //for desktop (Progman or WorkerW, depends on the system), we have to check 
-                    if (windowClass == WINDOW_CLASS_PROGMAN || windowClass == WINDOW_CLASS_WORKERW)
-                    {
-                        IntPtr hWndDesktop = FindWindowEx(hWnd, IntPtr.Zero, "SHELLDLL_DefView", null);
-                        hWndDesktop = FindWindowEx(hWndDesktop, IntPtr.Zero, "SysListView32", "FolderView");
-                        if (!hWndDesktop.Equals(IntPtr.Zero))
-                        {
-                            return false;
-                        }
-                    }
-
-                    Rectangle screenBounds = Screen.FromHandle(hWnd).Bounds;
-                    if ((appBounds.Bottom - appBounds.Top) == screenBounds.Height && (appBounds.Right - appBounds.Left) == screenBounds.Width)
-                    {
-                        return true;
-                    }
-                }
-            }
-
             return false;
         }
 
-        /// <summary>
-        ///     disable windows toolbar's control box
-        ///     this will also disable system menu with Alt+Space hotkey
-        /// </summary>
-        public static void DisableControlBox(Window win)
+        //if current active window is desktop or shell, exit early
+        if (hWnd.Equals(HWND_DESKTOP) || hWnd.Equals(HWND_SHELL))
         {
-            var hwnd = new WindowInteropHelper(win).Handle;
-            SetWindowLong(hwnd, GWL_STYLE, GetWindowLong(hwnd, GWL_STYLE) & ~WS_SYSMENU);
+            return false;
         }
 
-        /// <summary>
-        /// Transforms pixels to Device Independent Pixels used by WPF
-        /// </summary>
-        /// <param name="visual">current window, required to get presentation source</param>
-        /// <param name="unitX">horizontal position in pixels</param>
-        /// <param name="unitY">vertical position in pixels</param>
-        /// <returns>point containing device independent pixels</returns>
-        public static Point TransformPixelsToDIP(Visual visual, double unitX, double unitY)
+        StringBuilder sb = new StringBuilder(256);
+        GetClassName(hWnd, sb, sb.Capacity);
+        string windowClass = sb.ToString();
+
+        //for Win+Tab (Flip3D)
+        if (windowClass == WINDOW_CLASS_WINTAB)
         {
-            Matrix matrix;
-            var source = PresentationSource.FromVisual(visual);
-            if (source != null)
+            return false;
+        }
+
+        RECT appBounds;
+        GetWindowRect(hWnd, out appBounds);
+
+        //for console (ConsoleWindowClass), we have to check for negative dimensions
+        if (windowClass == WINDOW_CLASS_CONSOLE)
+        {
+            return appBounds.Top < 0 && appBounds.Bottom < 0;
+        }
+
+        //for desktop (Progman or WorkerW, depends on the system), we have to check
+        if (windowClass is WINDOW_CLASS_PROGMAN or WINDOW_CLASS_WORKERW)
+        {
+            IntPtr hWndDesktop = FindWindowEx(hWnd, IntPtr.Zero, "SHELLDLL_DefView", null);
+            hWndDesktop = FindWindowEx(hWndDesktop, IntPtr.Zero, "SysListView32", "FolderView");
+            if (!hWndDesktop.Equals(IntPtr.Zero))
             {
-                matrix = source.CompositionTarget.TransformFromDevice;
+                return false;
             }
-            else
-            {
-                using (var src = new HwndSource(new HwndSourceParameters()))
-                {
-                    matrix = src.CompositionTarget.TransformFromDevice;
-                }
-            }
-            return new Point((int)(matrix.M11 * unitX), (int)(matrix.M22 * unitY));
         }
 
+        Rectangle screenBounds = Screen.FromHandle(hWnd).Bounds;
+        return (appBounds.Bottom - appBounds.Top) == screenBounds.Height && (appBounds.Right - appBounds.Left) == screenBounds.Width;
+    }
 
-        [StructLayout(LayoutKind.Sequential)]
-        public struct RECT
+    /// <summary>
+    ///     disable windows toolbar's control box
+    ///     this will also disable system menu with Alt+Space hotkey
+    /// </summary>
+    public static void DisableControlBox(Window win)
+    {
+        var hwnd = new WindowInteropHelper(win).Handle;
+        SetWindowLong(hwnd, GWL_STYLE, GetWindowLong(hwnd, GWL_STYLE) & ~WS_SYSMENU);
+    }
+
+    /// <summary>
+    /// Transforms pixels to Device Independent Pixels used by WPF
+    /// </summary>
+    /// <param name="visual">current window, required to get presentation source</param>
+    /// <param name="unitX">horizontal position in pixels</param>
+    /// <param name="unitY">vertical position in pixels</param>
+    /// <returns>point containing device independent pixels</returns>
+    public static Point TransformPixelsToDIP(Visual visual, double unitX, double unitY)
+    {
+        Matrix matrix;
+        var source = PresentationSource.FromVisual(visual);
+        if (source is not null)
         {
-            public int Left;
-            public int Top;
-            public int Right;
-            public int Bottom;
+            matrix = source.CompositionTarget.TransformFromDevice;
         }
+        else
+        {
+            using var src = new HwndSource(new HwndSourceParameters());
+            matrix = src.CompositionTarget.TransformFromDevice;
+        }
+        return new Point((int)(matrix.M11 * unitX), (int)(matrix.M22 * unitY));
+    }
+
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct RECT
+    {
+        public int Left;
+        public int Top;
+        public int Right;
+        public int Bottom;
     }
 }
