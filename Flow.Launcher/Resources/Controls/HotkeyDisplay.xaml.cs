95ebac27b9617250b2d220b2a597bda1028246e7
DB p <onesound@oooz.net>
2024-04-17 13:43:54 +0900

    Add Hotkey Display

---
+++ Flow.Launcher/Resources/Controls/HotkeyDisplay.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Flow.Launcher.Resources.Controls
+{
+    public partial class HotkeyDisplay : UserControl
+    {
+        public HotkeyDisplay()
+        {
+            InitializeComponent();
+        }
+
+        public string Keys
+        {
+            get { return (string)GetValue(KeysValueProperty); }
+            set { SetValue(KeysValueProperty, value); }
+        }
+        public static readonly DependencyProperty KeysValueProperty =
+          DependencyProperty.Register("Keys", typeof(string), typeof(HotkeyDisplay), new PropertyMetadata(string.Empty));
+    }
+}
