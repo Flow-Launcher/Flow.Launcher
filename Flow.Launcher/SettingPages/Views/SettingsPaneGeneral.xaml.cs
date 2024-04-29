2285040857425c1127fb43c4f5283a40218968d5
Yusyuriv <Yusyuriv@users.noreply.github.com>
2024-04-29 12:25:10 +0600

    "General" pane in settings fully functional except for auto-updater and portable mode switches

--- Flow.Launcher/SettingPages/Views/SettingsPaneGeneral.xaml.cs
+++ Flow.Launcher/SettingPages/Views/SettingsPaneGeneral.xaml.cs
@@ -1,53 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Windows;
-using System.Windows.Navigation;
-using Flow.Launcher.Core.Plugin;
-using Flow.Launcher.Infrastructure;
-using Flow.Launcher.SettingPages.ViewModels;
 using Flow.Launcher.Infrastructure.UserSettings;
-using Path = System.IO.Path;
-using MessageBox = System.Windows.Forms.MessageBox;
 using Flow.Launcher.SettingPages.ViewModels;
-using System;
-using System.IO;
-using System.Windows;
-using System.Windows.Forms;
-using System.Windows.Navigation;
-using Flow.Launcher.Core.Plugin;
-using Flow.Launcher.Infrastructure.UserSettings;
-using Flow.Launcher.Plugin;
-using Flow.Launcher.SettingPages.ViewModels;
-using Flow.Launcher.ViewModel;
 
-namespace Flow.Launcher.SettingPages.Views
+namespace Flow.Launcher.SettingPages.Views;
+
+public partial class SettingsPaneGeneral
 {
-    public partial class SettingsPaneGeneral
+    private SettingsPaneGeneralViewModel _viewModel = null!;
+
+    protected override void OnNavigatedTo(NavigationEventArgs e)
     {
-        protected override void OnNavigatedTo(NavigationEventArgs e)
+        if (!IsInitialized)
         {
-            if (!IsInitialized)
-            {
-                if (e.ExtraData is not Settings settings)
-                    throw new ArgumentException("Settings is not passed to General page");
-                DataContext = new GeneralViewModel(settings);
-                InitializeComponent();
-            }
-            base.OnNavigatedTo(e);
+            if (e.ExtraData is not Settings settings)
+                throw new ArgumentException("Settings is not passed to General page");
+            _viewModel = new SettingsPaneGeneralViewModel(settings);
+            DataContext = _viewModel;
+            InitializeComponent();
         }
-
-
+        base.OnNavigatedTo(e);
     }
 }
