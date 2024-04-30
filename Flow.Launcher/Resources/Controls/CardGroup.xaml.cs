08687d63cb3511b45a791ea49b5d4e87d41c6d79
Yusyuriv <Yusyuriv@users.noreply.github.com>
2024-04-30 09:16:43 +0600

    Implement CardGroup control

---
+++ Flow.Launcher/Resources/Controls/CardGroup.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Flow.Launcher.Resources.Controls;
+
+public partial class CardGroup : UserControl
+{
+    public new ObservableCollection<Card> Content
+    {
+        get { return (ObservableCollection<Card>)GetValue(ContentProperty); }
+        set { SetValue(ContentProperty, value); }
+    }
+
+    public static readonly DependencyProperty ContentProperty =
+        DependencyProperty.Register(nameof(Content), typeof(ObservableCollection<Card>), typeof(CardGroup));
+
+    public CardGroup()
+    {
+        InitializeComponent();
+        DataContext = this;
+        Content = new ObservableCollection<Card>();
+    }
+}
