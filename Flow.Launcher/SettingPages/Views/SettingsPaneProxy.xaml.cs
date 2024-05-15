f7b2d9df3bfda501fe4b2e12076ac4fd588186e4
Yusyuriv <Yusyuriv@users.noreply.github.com>
2024-05-16 00:53:12 +0600

    Implement settings proxy pane

--- Flow.Launcher/SettingPages/Views/SettingsPaneProxy.xaml.cs
+++ Flow.Launcher/SettingPages/Views/SettingsPaneProxy.xaml.cs
@@ -1,28 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Flow.Launcher.SettingPages.ViewModels;
 
-namespace Flow.Launcher.SettingPages.Views
+namespace Flow.Launcher.SettingPages.Views;
+
+public partial class SettingsPaneProxy
 {
-    /// <summary>
-    /// Proxy.xaml에 대한 상호 작용 논리
-    /// </summary>
-    public partial class SettingsPaneProxy : Page
+    private SettingsPaneProxyViewModel _viewModel = null!;
+
+    protected override void OnNavigatedTo(NavigationEventArgs e)
     {
-        public SettingsPaneProxy()
+        if (!IsInitialized)
         {
+            if (e.ExtraData is not SettingWindow.PaneData { Settings: { } settings, Updater: { } updater })
+                throw new ArgumentException($"Settings are required for {nameof(SettingsPaneProxy)}.");
+            _viewModel = new SettingsPaneProxyViewModel(settings, updater);
+            DataContext = _viewModel;
             InitializeComponent();
         }
+
+        base.OnNavigatedTo(e);
     }
 }
