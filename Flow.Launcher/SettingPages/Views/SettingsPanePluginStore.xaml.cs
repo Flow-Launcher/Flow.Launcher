3bcb5da99c7117241694ad1b742b3a62c20b39d0
Yusyuriv <Yusyuriv@users.noreply.github.com>
2024-05-15 22:56:58 +0600

    Implement plugin store settings pane

--- Flow.Launcher/SettingPages/Views/SettingsPanePluginStore.xaml.cs
+++ Flow.Launcher/SettingPages/Views/SettingsPanePluginStore.xaml.cs
@@ -1,28 +1,66 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
+using System.ComponentModel;
 using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Flow.Launcher.Core.Plugin;
+using Flow.Launcher.SettingPages.ViewModels;
+using Flow.Launcher.ViewModel;
 
-namespace Flow.Launcher.SettingPages.Views
+namespace Flow.Launcher.SettingPages.Views;
+
+public partial class SettingsPanePluginStore
 {
-    /// <summary>
-    /// PluginStore.xaml에 대한 상호 작용 논리
-    /// </summary>
-    public partial class SettingsPanePluginStore : Page
+    private SettingsPanePluginStoreViewModel _viewModel = null!;
+
+    protected override void OnNavigatedTo(NavigationEventArgs e)
     {
-        public SettingsPanePluginStore()
+        if (!IsInitialized)
         {
+            if (e.ExtraData is not SettingWindow.PaneData { Settings: { } settings })
+                throw new ArgumentException($"Settings are required for {nameof(SettingsPanePluginStore)}.");
+            _viewModel = new SettingsPanePluginStoreViewModel();
+            DataContext = _viewModel;
             InitializeComponent();
         }
+        _viewModel.PropertyChanged += ViewModel_PropertyChanged;
+        base.OnNavigatedTo(e);
+    }
+
+    private void ViewModel_PropertyChanged(object sender, PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName == nameof(SettingsPanePluginStoreViewModel.FilterText))
+        {
+            ((CollectionViewSource)FindResource("PluginStoreCollectionView")).View.Refresh();
+        }
+    }
+
+    protected override void OnNavigatingFrom(NavigatingCancelEventArgs e)
+    {
+        _viewModel.PropertyChanged -= ViewModel_PropertyChanged;
+        base.OnNavigatingFrom(e);
+    }
+
+    private void SettingsPanePlugins_OnKeyDown(object sender, KeyEventArgs e)
+    {
+        if (e.Key is not Key.F || Keyboard.Modifiers is not ModifierKeys.Control) return;
+        PluginStoreFilterTextbox.Focus();
+    }
+
+    private void Hyperlink_OnRequestNavigate(object sender, RequestNavigateEventArgs e)
+    {
+        PluginManager.API.OpenUrl(e.Uri.AbsoluteUri);
+        e.Handled = true;
+    }
+
+    private void PluginStoreCollectionView_OnFilter(object sender, FilterEventArgs e)
+    {
+        if (e.Item is not PluginStoreItemViewModel plugin)
+        {
+            e.Accepted = false;
+            return;
+        }
+
+        e.Accepted = _viewModel.SatisfiesFilter(plugin);
     }
 }
