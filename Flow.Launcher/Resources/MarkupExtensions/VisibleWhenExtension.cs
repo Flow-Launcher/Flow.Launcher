4be61880dd1e4cc5b21aa6b92d2c367cc13d0bbd
Yusyuriv <Yusyuriv@users.noreply.github.com>
2024-05-16 22:57:10 +0600

    Add CollapsedWhen and VisibleWhen markup extensions

---
+++ Flow.Launcher/Resources/MarkupExtensions/VisibleWhenExtension.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Data;
+
+namespace Flow.Launcher.Resources.MarkupExtensions;
+
+#nullable enable
+
+public class VisibleWhenExtension : CollapsedWhenExtension
+{
+    protected override Visibility DefaultVisibility => Visibility.Collapsed;
+    protected override Visibility InvertedVisibility => Visibility.Visible;
+
+    public VisibleWhenExtension(Binding when) : base(when) { }
+
+    public VisibleWhenExtension(Binding when, object? isEqualTo) : base(when, isEqualTo) { }
+}
