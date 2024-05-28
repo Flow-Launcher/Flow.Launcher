666211dd4cecae901f0a5509447fc07e2a72feaa
Jeremy <jeremy24wu@gmail.com>
2024-05-28 21:27:48 +1000

    add plugin support for external preview

---
+++ Flow.Launcher.Plugin/Interfaces/IAsyncExternalPreview.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+
+namespace Flow.Launcher.Plugin
+{
+    public interface IAsyncExternalPreview: IFeatures
+    {
+        public Task TogglePreviewAsync(string path);
+
+        public Task OpenPreviewAsync(string path, bool sendFailToast = true);
+
+        public Task ClosePreviewAsync();
+
+        public Task SwitchPreviewAsync(string path, bool sendFailToast = true);
+    }
+}
