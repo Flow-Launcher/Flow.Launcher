29be50478984e0b70f12f89e477188a581131ab8
Yusyuriv <Yusyuriv@users.noreply.github.com>
2024-05-30 20:49:36 +0600

    Implement temporary result list height increase when preview is on

--- Flow.Launcher/ViewModel/MainViewModel.cs
+++ Flow.Launcher/ViewModel/MainViewModel.cs
@@ -143,15 +143,21 @@ namespace Flow.Launcher.ViewModel
 
             ContextMenu = new ResultsViewModel(Settings)
             {
-                LeftClickResultCommand = OpenResultCommand, RightClickResultCommand = LoadContextMenuCommand
+                LeftClickResultCommand = OpenResultCommand,
+                RightClickResultCommand = LoadContextMenuCommand,
+                IsPreviewOn = Settings.AlwaysPreview
             };
             Results = new ResultsViewModel(Settings)
             {
-                LeftClickResultCommand = OpenResultCommand, RightClickResultCommand = LoadContextMenuCommand
+                LeftClickResultCommand = OpenResultCommand,
+                RightClickResultCommand = LoadContextMenuCommand,
+                IsPreviewOn = Settings.AlwaysPreview
             };
             History = new ResultsViewModel(Settings)
             {
-                LeftClickResultCommand = OpenResultCommand, RightClickResultCommand = LoadContextMenuCommand
+                LeftClickResultCommand = OpenResultCommand,
+                RightClickResultCommand = LoadContextMenuCommand,
+                IsPreviewOn = Settings.AlwaysPreview
             };
             _selectedResults = Results;
 
@@ -586,6 +592,10 @@ namespace Flow.Launcher.ViewModel
             {
                 HidePreview();
             }
+
+            ContextMenu.IsPreviewOn = PreviewVisible;
+            History.IsPreviewOn = PreviewVisible;
+            Results.IsPreviewOn = PreviewVisible;
         }
 
         private void ShowPreview()
