6807afbe6d753eed6984e1b1ab6019e2864767cd
VictoriousRaptor <10308169+VictoriousRaptor@users.noreply.github.com>
2024-06-02 14:03:00 +0800

    Remove unused alphabet arg in PublicAPIInstance

--- Flow.Launcher/PublicAPIInstance.cs
+++ Flow.Launcher/PublicAPIInstance.cs
@@ -32,15 +32,13 @@ namespace Flow.Launcher
     {
         private readonly SettingWindowViewModel _settingsVM;
         private readonly MainViewModel _mainVM;
-        private readonly DoublePinAlphabet _alphabet;
 
         #region Constructor
 
-        public PublicAPIInstance(SettingWindowViewModel settingsVM, MainViewModel mainVM, DoublePinAlphabet alphabet)
+        public PublicAPIInstance(SettingWindowViewModel settingsVM, MainViewModel mainVM)
         {
             _settingsVM = settingsVM;
             _mainVM = mainVM;
-            _alphabet = alphabet;
             GlobalHotkey.hookedKeyboardCallback = KListener_hookedKeyboardCallback;
             WebRequest.RegisterPrefix("data", new DataWebRequestFactory());
         }
