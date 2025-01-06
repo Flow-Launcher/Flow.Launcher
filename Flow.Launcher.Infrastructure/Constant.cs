46c2c1f6683e50652afbfa541f187d15fb6275b6
Jack251970 <1160210343@qq.com>
2025-01-06 19:20:47 +0800

    Change system language code to lowercase

--- Flow.Launcher.Infrastructure/Constant.cs
+++ Flow.Launcher.Infrastructure/Constant.cs
@@ -53,6 +53,6 @@ namespace Flow.Launcher.Infrastructure
         public const string GitHub = "https://github.com/Flow-Launcher/Flow.Launcher";
         public const string Docs = "https://flowlauncher.com/docs";
 
-        public const string SystemLanguageCode = "System";
+        public const string SystemLanguageCode = "system";
     }
 }
