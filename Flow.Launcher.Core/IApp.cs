8b910500c6f3a2fe170cdc8261c6d0add722b744
Jack251970 <1160210343@qq.com>
2025-01-09 21:43:33 +0800

    Add IApp & AppExtensions for accessing the properties & functions from anywhere in the application

---
+++ Flow.Launcher.Core/IApp.cs
@@ -0,0 +1,13 @@
+﻿using Flow.Launcher.Plugin;
+
+namespace Flow.Launcher.Core
+{
+    /// <summary>
+    /// Interface for the current application singleton object exposing the properties
+    /// and functions that can be accessed from anywhere in the application.
+    /// </summary>
+    public interface IApp
+    {
+        public IPublicAPI PublicAPI { get; }
+    }
+}
