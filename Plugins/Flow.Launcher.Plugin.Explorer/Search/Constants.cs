a3cc5e25f9e8cad6cde6a7360a10da2400589f10
Jack251970 <1160210343@qq.com>
2025-02-07 16:05:35 +0800

    Fix unix directory seperator issue

--- Plugins/Flow.Launcher.Plugin.Explorer/Search/Constants.cs
+++ Plugins/Flow.Launcher.Plugin.Explorer/Search/Constants.cs
@@ -31,6 +31,8 @@ namespace Flow.Launcher.Plugin.Explorer.Search
 
         internal const string DefaultContentSearchActionKeyword = "doc:";
 
+        internal const char UnixDirectorySeparator = '/';
+
         internal const char DirectorySeparator = '\\';
 
         internal const string WindowsIndexingOptions = "srchadmin.dll";
