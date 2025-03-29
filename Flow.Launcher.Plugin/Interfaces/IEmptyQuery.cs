e0721aae1464ba54a2c8fb2591cb7fd15660bc61
Jack251970 <1160210343@qq.com>
2025-03-29 22:03:30 +0800

    Add new interfaces

---
+++ Flow.Launcher.Plugin/Interfaces/IEmptyQuery.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Flow.Launcher.Plugin
+{
+    /// <summary>
+    /// Synchronous Query Model for Flow Launcher When Query Text is Empty
+    /// <para>
+    /// If the Querying method requires high IO transmission
+    /// or performaing CPU intense jobs (performing better with cancellation), please try the IAsyncEmptyQuery interface
+    /// </para>
+    /// </summary>
+    public interface IEmptyQuery : IAsyncEmptyQuery
+    {
+        /// <summary>
+        /// Querying When Query Text is Empty
+        /// <para>
+        /// This method will be called within a Task.Run,
+        /// so please avoid synchrously wait for long.
+        /// </para>
+        /// </summary>
+        /// <returns></returns>
+        List<Result> EmptyQuery();
+
+        Task<List<Result>> IAsyncEmptyQuery.EmptyQueryAsync(CancellationToken token) => Task.Run(EmptyQuery);
+    }
+}
