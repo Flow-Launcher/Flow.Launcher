a35fb347609bbb59da5670f2d349dbc4115b0565
DB p <onesound@oooz.net>
2025-03-30 15:37:22 +0900

    - Reordered the settings items and changed some items to expandable card type - Replaced the theme creation guide link with the Theme Builder link - Fix Padding for InsideFit card

--- Flow.Launcher/SettingPages/ViewModels/SettingsPaneThemeViewModel.cs
+++ Flow.Launcher/SettingPages/ViewModels/SettingsPaneThemeViewModel.cs
@@ -24,7 +24,7 @@ public partial class SettingsPaneThemeViewModel : BaseModel
     public Settings Settings { get; }
     private readonly Theme _theme = Ioc.Default.GetRequiredService<Theme>();
 
-    public static string LinkHowToCreateTheme => @"https://flowlauncher.com/docs/#/how-to-create-a-theme";
+    public static string LinkHowToCreateTheme => @"https://www.flowlauncher.com/theme-builder/";
     public static string LinkThemeGallery => "https://github.com/Flow-Launcher/Flow.Launcher/discussions/1438";
 
     private List<Theme.ThemeData> _themes;
