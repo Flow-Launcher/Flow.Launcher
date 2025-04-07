65f48e3069c173e3f2be4ff255d405112dd0f29f
Jack251970 <1160210343@qq.com>
2025-04-07 14:54:09 +0800

    Force priority to 0 when inputing empty

--- Flow.Launcher/Resources/Controls/InstalledPluginDisplay.xaml.cs
+++ Flow.Launcher/Resources/Controls/InstalledPluginDisplay.xaml.cs
@@ -1,9 +1,19 @@
-﻿namespace Flow.Launcher.Resources.Controls;
+﻿using ModernWpf.Controls;
+
+namespace Flow.Launcher.Resources.Controls;
 
 public partial class InstalledPluginDisplay
 {
     public InstalledPluginDisplay()
     {
         InitializeComponent();
+    }
+
+    private void NumberBox_OnValueChanged(NumberBox sender, NumberBoxValueChangedEventArgs args)
+    {
+        if (double.IsNaN(args.NewValue))
+        {
+            sender.Value = 0;
+        }
     }
 }
