c41d02127206047641b5e258366dfb1f4d2ee4ab
Jack251970 <1160210343@qq.com>
2025-04-09 13:01:29 +0800

    Add obsolete warning

--- Flow.Launcher.Core/Resource/LocalizationConverter.cs
+++ Flow.Launcher.Core/Resource/LocalizationConverter.cs
@@ -6,6 +6,7 @@ using System.Windows.Data;
 
 namespace Flow.Launcher.Core.Resource
 {
+    [Obsolete("LocalizationConverter is obsolete. Use with Flow.Launcher.Localization NuGet package instead.")]
     public class LocalizationConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
