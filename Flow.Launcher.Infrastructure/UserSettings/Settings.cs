086f982a3ccb2a9818bc153bb0c701044c7ff645
Jack251970 <1160210343@qq.com>
2025-04-20 15:35:34 +0800

    Add auto switch option back

--- Flow.Launcher.Infrastructure/UserSettings/Settings.cs
+++ Flow.Launcher.Infrastructure/UserSettings/Settings.cs
@@ -232,8 +232,6 @@ namespace Flow.Launcher.Infrastructure.UserSettings
 
         public bool EnableQuickSwitch { get; set; } = true;
 
-        // TODO: TODO: Due to many issues, this option is removed from FL
-        // Please see https://github.com/Flow-Launcher/Flow.Launcher/pull/1018
         public bool AutoQuickSwitch { get; set; } = false;
 
         public bool ShowQuickSwitchWindow { get; set; } = true;
