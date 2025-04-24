94ff72c8f27d48201bc195ef32bdf801ddeb4d13
Jack251970 <1160210343@qq.com>
2025-04-24 19:49:03 +0800

    Check startup only for Release

--- Flow.Launcher/App.xaml.cs
+++ Flow.Launcher/App.xaml.cs
@@ -201,6 +201,10 @@ namespace Flow.Launcher
 
 #pragma warning restore VSTHRD100 // Avoid async void methods
 
+        /// <summary>
+        /// Check startup only for Release
+        /// </summary>
+        [Conditional("RELEASE")]
         private void AutoStartup()
         {
             // we try to enable auto-startup on first launch, or reenable if it was removed
