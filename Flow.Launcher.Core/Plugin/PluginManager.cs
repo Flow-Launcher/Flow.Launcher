7509a86cbfde81f7a0696049b2e0438e6bff4f9a
Jack251970 <1160210343@qq.com>
2025-05-13 09:47:46 +0800

    Use skip message for failed initialization when plugin is already disabled

--- Flow.Launcher.Core/Plugin/PluginManager.cs
+++ Flow.Launcher.Core/Plugin/PluginManager.cs
@@ -219,17 +219,18 @@ namespace Flow.Launcher.Core.Plugin
                 }
                 catch (Exception e)
                 {
-                    API.LogException(ClassName, $"Fail to Init plugin: {pair.Metadata.Name}", e);
                     if (pair.Metadata.Disabled && pair.Metadata.HomeDisabled)
                     {
                         // If this plugin is already disabled, do not show error message again
                         // Or else it will be shown every time
+                        API.LogDebug(ClassName, $"Skip init for <{pair.Metadata.Name}>");
                     }
                     else
                     {
                         pair.Metadata.Disabled = true;
                         pair.Metadata.HomeDisabled = true;
                         failedPlugins.Enqueue(pair);
+                        API.LogException(ClassName, $"Fail to Init plugin: {pair.Metadata.Name}", e);
                     }
                 }
             }));
