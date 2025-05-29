aefce9f1a5092a9f85023131d6f622c68231b52e
Jack251970 <1160210343@qq.com>
2025-05-29 15:18:14 +0800

    Add helper from ModernWPF

---
+++ Flow.Launcher/Helper/BorderHelper.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Flow.Launcher.Helper;
+
+public static class BorderHelper
+{
+    #region Child
+
+    public static readonly DependencyProperty ChildProperty =
+        DependencyProperty.RegisterAttached(
+            "Child",
+            typeof(UIElement),
+            typeof(BorderHelper),
+            new PropertyMetadata(default(UIElement), OnChildChanged));
+
+    public static UIElement GetChild(Border border)
+    {
+        return (UIElement)border.GetValue(ChildProperty);
+    }
+
+    public static void SetChild(Border border, UIElement value)
+    {
+        border.SetValue(ChildProperty, value);
+    }
+
+    private static void OnChildChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        ((Border)d).Child = (UIElement)e.NewValue;
+    }
+
+    #endregion
+}
