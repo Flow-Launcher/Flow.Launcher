ce50d9876f4f944a2c936b24ece2ac34a03983dc
Jack Ye <1160210343@qq.com>
2025-06-09 16:27:19 +0800

    Merge pull request #3668 from Flow-Launcher/derive_class_save
    Fix Derive Class Save Method Calling Issue

--- Flow.Launcher/PublicAPIInstance.cs
+++ Flow.Launcher/PublicAPIInstance.cs
@@ -276,7 +276,7 @@ namespace Flow.Launcher
         public void LogException(string className, string message, Exception e, [CallerMemberName] string methodName = "") =>
             Log.Exception(className, message, e, methodName);
 
-        private readonly ConcurrentDictionary<Type, object> _pluginJsonStorages = new();
+        private readonly ConcurrentDictionary<Type, ISavable> _pluginJsonStorages = new();
 
         public void RemovePluginSettings(string assemblyName)
         {
@@ -294,10 +294,9 @@ namespace Flow.Launcher
 
         public void SavePluginSettings()
         {
-            foreach (var value in _pluginJsonStorages.Values)
+            foreach (var savable in _pluginJsonStorages.Values)
             {
-                var savable = value as ISavable;
-                savable?.Save();
+                savable.Save();
             }
         }
 
@@ -496,7 +495,7 @@ namespace Flow.Launcher
         public bool SetCurrentTheme(ThemeData theme) =>
             Theme.ChangeTheme(theme.FileNameWithoutExtension);
 
-        private readonly ConcurrentDictionary<(string, string, Type), object> _pluginBinaryStorages = new();
+        private readonly ConcurrentDictionary<(string, string, Type), ISavable> _pluginBinaryStorages = new();
 
         public void RemovePluginCaches(string cacheDirectory)
         {
@@ -513,10 +512,9 @@ namespace Flow.Launcher
 
         public void SavePluginCaches()
         {
-            foreach (var value in _pluginBinaryStorages.Values)
+            foreach (var savable in _pluginBinaryStorages.Values)
             {
-                var savable = value as ISavable;
-                savable?.Save();
+                savable.Save();
             }
         }
 
