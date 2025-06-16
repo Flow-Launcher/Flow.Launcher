30d10bb37f6fb903f4d7c9cfbea82571eb9372b7
Jack251970 <1160210343@qq.com>
2025-06-16 21:11:50 +0800

    Add try catch for register key check

--- Flow.Launcher/Helper/WindowsMediaPlayerHelper.cs
+++ Flow.Launcher/Helper/WindowsMediaPlayerHelper.cs
@@ -1,11 +1,19 @@
 ﻿using Microsoft.Win32;
 
 namespace Flow.Launcher.Helper;
+
 internal static class WindowsMediaPlayerHelper
 {
     internal static bool IsWindowsMediaPlayerInstalled()
     {
-        using var key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\MediaPlayer");
-        return key?.GetValue("Installation Directory") != null;
+        try
+        {
+            using var key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\MediaPlayer");
+            return key?.GetValue("Installation Directory") != null;
+        }
+        catch
+        {
+            return false;
+        }
     }
 }
