e12c2be419d3e89dd5f3b003d63eac1281016226
Koisu <koisuspotify@gmail.com>
2025-06-23 18:57:41 -0700

    polished ui

--- Plugins/Flow.Launcher.Plugin.Explorer/Views/RenameFile.xaml.cs
+++ Plugins/Flow.Launcher.Plugin.Explorer/Views/RenameFile.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Threading;
 using CommunityToolkit.Mvvm.ComponentModel;
 using Flow.Launcher.Plugin.Explorer.Helper;
 using Microsoft.VisualBasic.Logging;
@@ -37,24 +38,44 @@ namespace Flow.Launcher.Plugin.Explorer.Views
         public RenameFile(IPublicAPI api, FileSystemInfo info)
         {
             _api = api;
-
-
-            InitializeComponent();
-
-            RenameTb.Focus();
-
-            ShowInTaskbar = false;
-
-            RenameTb.SelectAll();
-
-
             _info = info;
             _oldFilePath = _info.FullName;
             NewFileName = _info.Name;
 
+            InitializeComponent();
+
+            
+
+            ShowInTaskbar = false;
+
+            
+
+            RenameTb.Focus();
+            
+
 
         }
+        /// <summary>
+        /// https://stackoverflow.com/a/59560352/24045055
+        /// </summary>
 
+        private async void SelectAll_OnTextBoxGotFocus(object sender, RoutedEventArgs e)
+        {
+            
+            var textBox = sender as TextBox;
+            if (_info is DirectoryInfo)
+            {
+                await Application.Current.Dispatcher.InvokeAsync(textBox.SelectAll, DispatcherPriority.Background);
+            }
+            // select everything but the extension
+            else if (_info is FileInfo info)
+            {
+                string properName = Path.GetFileNameWithoutExtension(info.Name);
+                Application.Current.Dispatcher.Invoke(textBox.Select, DispatcherPriority.Background, textBox.Text.IndexOf(properName), properName.Length );
+
+            }
+            
+        }
         private void OnDoneButtonClick(object sender, RoutedEventArgs e)
         {
             RenameThing.Rename(NewFileName, _info, _api);
