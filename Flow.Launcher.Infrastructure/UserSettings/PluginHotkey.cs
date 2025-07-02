476ad9be9b85b28191584bbed36c0ba71094347f
Jack251970 <1160210343@qq.com>
2025-07-02 15:17:35 +0800

    Add constructor for CustomPluginHotkey

--- Flow.Launcher.Infrastructure/UserSettings/PluginHotkey.cs
+++ Flow.Launcher.Infrastructure/UserSettings/PluginHotkey.cs
@@ -8,6 +8,12 @@ namespace Flow.Launcher.Infrastructure.UserSettings
         public string Hotkey { get; set; }
         public string ActionKeyword { get; set; }
 
+        public CustomPluginHotkey(string hotkey, string actionKeyword)
+        {
+            Hotkey = hotkey;
+            ActionKeyword = actionKeyword;
+        }
+
         public override bool Equals(object other)
         {
             if (other is CustomPluginHotkey otherHotkey)
