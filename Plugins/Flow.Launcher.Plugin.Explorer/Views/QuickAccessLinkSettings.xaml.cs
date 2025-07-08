bad7db69dc252e96de5002f42ede8ddebcf24c65
Jack251970 <1160210343@qq.com>
2025-07-08 09:45:54 +0800

    Improve code quality

--- Plugins/Flow.Launcher.Plugin.Explorer/Views/QuickAccessLinkSettings.xaml.cs
+++ Plugins/Flow.Launcher.Plugin.Explorer/Views/QuickAccessLinkSettings.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Windows;
 using System.Windows.Forms;
@@ -190,13 +191,13 @@ public partial class QuickAccessLinkSettings
     private static ResultType GetResultType(string path)
     {
         // Check if the path is a file or folder
-        if (System.IO.File.Exists(path))
+        if (File.Exists(path))
         {
             return ResultType.File;
         }
-        else if (System.IO.Directory.Exists(path))
+        else if (Directory.Exists(path))
         {
-            if (string.Equals(System.IO.Path.GetPathRoot(path), path, StringComparison.OrdinalIgnoreCase))
+            if (string.Equals(Path.GetPathRoot(path), path, StringComparison.OrdinalIgnoreCase))
             {
                 return ResultType.Volume;
             }
