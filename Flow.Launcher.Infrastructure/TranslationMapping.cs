2316c36a051034b5dba35aa36651b811f7eb2280
VictoriousRaptor <10308169+VictoriousRaptor@users.noreply.github.com>
2025-07-13 23:29:41 +0800

    Improve performance and readablility

--- Flow.Launcher.Infrastructure/TranslationMapping.cs
+++ Flow.Launcher.Infrastructure/TranslationMapping.cs
@@ -6,31 +6,31 @@ namespace Flow.Launcher.Infrastructure
 {
     public class TranslationMapping
     {
-        private bool constructed;
+        private bool _isConstructed;
 
         // Assuming one original item maps to multi translated items  
         // list[i] is the last translated index + 1 of original index i  
-        private readonly List<int> originalToTranslated = new List<int>();
+        private readonly List<int> _originalToTranslated = new();
 
         public void AddNewIndex(int translatedIndex, int length)
         {
-            if (constructed)
-                throw new InvalidOperationException("Mapping shouldn't be changed after constructed");
+            if (_isConstructed)
+                throw new InvalidOperationException("Mapping shouldn't be changed after construction");
 
-            originalToTranslated.Add(translatedIndex + length);
+            _originalToTranslated.Add(translatedIndex + length);
         }
 
         public int MapToOriginalIndex(int translatedIndex)
         {
-            int loc = originalToTranslated.BinarySearch(translatedIndex);
-            return loc >= 0 ? loc : ~loc;
+            var searchResult = _originalToTranslated.BinarySearch(translatedIndex);
+            return searchResult >= 0 ? searchResult : ~searchResult;
         }
 
-        public void endConstruct()
+        public void EndConstruct()
         {
-            if (constructed)
+            if (_isConstructed)
                 throw new InvalidOperationException("Mapping has already been constructed");
-            constructed = true;
+            _isConstructed = true;
         }
     }
 }
