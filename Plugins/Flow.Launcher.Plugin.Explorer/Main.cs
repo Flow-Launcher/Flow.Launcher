4d44ec40bd5c116f417a00ce5eb3042675a40272
Jeremy <jeremy24wu@gmail.com>
2025-07-13 22:43:08 +1000

    Revert "Merge pull request #3810 from Flow-Launcher/quick_access_link_type"
    This reverts commit 575236d0f5ccfdec63bafddcc3688fa575f333c8.

--- Plugins/Flow.Launcher.Plugin.Explorer/Main.cs
+++ Plugins/Flow.Launcher.Plugin.Explorer/Main.cs
@@ -1,4 +1,4 @@
-using Flow.Launcher.Plugin.Explorer.Helper;
+﻿using Flow.Launcher.Plugin.Explorer.Helper;
 using Flow.Launcher.Plugin.Explorer.Search;
 using Flow.Launcher.Plugin.Explorer.Search.Everything;
 using Flow.Launcher.Plugin.Explorer.ViewModels;
@@ -94,18 +94,6 @@ namespace Flow.Launcher.Plugin.Explorer
         public string GetTranslatedPluginDescription()
         {
             return Context.API.GetTranslation("plugin_explorer_plugin_description");
-        }
-
-        private static void FillQuickAccessLinkNames()
-        {
-            // Legacy version does not have names for quick access links, so we fill them with the path name.
-            foreach (var link in Settings.QuickAccessLinks)
-            {
-                if (string.IsNullOrWhiteSpace(link.Name))
-                {
-                    link.Name = link.Path.GetPathName();
-                }
-            }
         }
     }
 }
