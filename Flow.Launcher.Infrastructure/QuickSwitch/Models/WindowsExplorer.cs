085d625c39334fe72ba5cf6bd9af4cc129b3702b
Jack251970 <1160210343@qq.com>
2025-07-17 15:08:46 +0800

    Support active explorer path

--- Flow.Launcher.Infrastructure/QuickSwitch/Models/WindowsExplorer.cs
+++ Flow.Launcher.Infrastructure/QuickSwitch/Models/WindowsExplorer.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Runtime.InteropServices;
+using System.Threading;
 using Flow.Launcher.Plugin;
 using Windows.Win32;
+using Windows.Win32.Foundation;
 using Windows.Win32.System.Com;
 using Windows.Win32.UI.Shell;
 
@@ -28,10 +30,10 @@ namespace Flow.Launcher.Infrastructure.QuickSwitch.Models
 
                         if (explorer.HWND != hwnd) return true;
 
-                        explorerWindow = new WindowsExplorerWindow(hwnd, explorer);
+                        explorerWindow = new WindowsExplorerWindow(hwnd);
                         return false;
                     }
-                    catch (COMException)
+                    catch
                     {
                         // Ignored
                     }
@@ -42,7 +44,7 @@ namespace Flow.Launcher.Infrastructure.QuickSwitch.Models
             return explorerWindow;
         }
 
-        private static unsafe void EnumerateShellWindows(Func<object, bool> action)
+        internal static unsafe void EnumerateShellWindows(Func<object, bool> action)
         {
             // Create an instance of ShellWindows
             var clsidShellWindows = new Guid("9BA05972-F6A8-11CF-A442-00A0C90A8F39"); // ShellWindowsClass
@@ -80,40 +82,119 @@ namespace Flow.Launcher.Infrastructure.QuickSwitch.Models
     {
         public IntPtr Handle { get; }
 
-        private static IWebBrowser2 _explorerView = null;
+        private static Guid _shellBrowserGuid = typeof(IShellBrowser).GUID;
 
-        internal WindowsExplorerWindow(IntPtr handle, IWebBrowser2 explorerView)
+        internal WindowsExplorerWindow(IntPtr handle)
         {
             Handle = handle;
-            _explorerView = explorerView;
         }
 
         public string GetExplorerPath()
         {
-            if (_explorerView == null) return null;
+            if (Handle == IntPtr.Zero) return null;
 
-            object document = null;
-            try
+            var activeTabHandle = GetActiveTabHandle(new(Handle));
+            if (activeTabHandle.IsNull) return null;
+
+            var window = GetExplorerByTabHandle(activeTabHandle);
+            if (window == null) return null;
+
+            var path = GetLocation(window);
+            return path;
+        }
+
+        public void Dispose()
+        {
+
+        }
+
+        #region Helper Methods
+
+        // Inspired by: https://github.com/w4po/ExplorerTabUtility
+
+        private static HWND GetActiveTabHandle(HWND windowHandle)
+        {
+            // Active tab always at the top of the z-index, so it is the first child of the ShellTabWindowClass.
+            var activeTab = PInvoke.FindWindowEx(windowHandle, HWND.Null, "ShellTabWindowClass", null);
+            return activeTab;
+        }
+
+        private static IWebBrowser2 GetExplorerByTabHandle(HWND tabHandle)
+        {
+            if (tabHandle.IsNull) return null;
+
+            IWebBrowser2 window = null;
+            WindowsExplorer.EnumerateShellWindows((shellWindow) =>
             {
-                if (_explorerView != null)
+                try
                 {
-                    // Use dynamic here because using IWebBrower2.Document can cause exception here:
-                    // System.Runtime.InteropServices.InvalidOleVariantTypeException: 'Specified OLE variant is invalid.'
-                    dynamic explorerView = _explorerView;
-                    document = explorerView.Document;
+                    return StartSTAThread(() =>
+                    {
+                        if (shellWindow is not IWebBrowser2 explorer) return true;
+
+                        if (explorer is not IServiceProvider sp) return true;
+
+                        sp.QueryService(ref _shellBrowserGuid, ref _shellBrowserGuid, out var shellBrowser);
+                        if (shellBrowser == null) return true;
+
+                        try
+                        {
+                            shellBrowser.GetWindow(out var hWnd); // Must execute in STA thread to get this hWnd
+
+                            if (hWnd == tabHandle)
+                            {
+                                window = explorer;
+                                return false;
+                            }
+                        }
+                        catch
+                        {
+                            // Ignored   
+                        }
+                        finally
+                        {
+                            Marshal.ReleaseComObject(shellBrowser);
+                        }
+
+                        return true;
+                    }) ?? true;
                 }
-            }
-            catch (COMException)
-            {
-                return null;
-            }
+                catch
+                {
+                    // Ignored
+                }
 
-            if (document is not IShellFolderViewDual2 folderView)
-            {
-                return null;
-            }
+                return true;
+            });
 
-            string path;
+            return window;
+        }
+
+        private static bool? StartSTAThread(Func<bool> action)
+        {
+            bool? result = null;
+            var thread = new Thread(() =>
+            {
+                result = action();
+            })
+            {
+                IsBackground = true
+            };
+            thread.SetApartmentState(ApartmentState.STA);
+            thread.Start();
+            thread.Join();
+            return result;
+        }
+
+        private static string GetLocation(IWebBrowser2 window)
+        {
+            var path = window.LocationURL.ToString();
+            if (!string.IsNullOrWhiteSpace(path)) return NormalizeLocation(path);
+
+            // Recycle Bin, This PC, etc
+            if (window.Document is not IShellFolderViewDual folderView) return null;
+
+            // Attempt to get the path from the folder view
             try
             {
                 // CSWin32 Folder does not have Self, so we need to use dynamic type here
@@ -123,40 +204,57 @@ namespace Flow.Launcher.Infrastructure.QuickSwitch.Models
                 // Access the Self property via dynamic binding
                 dynamic folderItem = folder.Self;
 
-                // Check if the item is part of the file system
-                if (folderItem != null && folderItem.IsFileSystem)
-                {
-                    path = folderItem.Path;
-                }
-                else
-                {
-                    // Handle non-file system paths (e.g., virtual folders)
-                    path = string.Empty;
-                }
+                // Get path from the folder item
+                path = folderItem.Path;
             }
             catch
             {
                 return null;
             }
 
-            return path;
+            return NormalizeLocation(path);
         }
 
-        public void Dispose()
+        private static string NormalizeLocation(string location)
         {
-            // Release ComObjects
-            try
-            {
-                if (_explorerView != null)
-                {
-                    Marshal.ReleaseComObject(_explorerView);
-                    _explorerView = null;
-                }
-            }
-            catch (COMException)
-            {
-                _explorerView = null;
-            }
+            if (location.IndexOf('%') > -1)
+                location = Environment.ExpandEnvironmentVariables(location);
+
+            if (location.StartsWith("::", StringComparison.Ordinal))
+                location = $"shell:{location}";
+
+            else if (location.StartsWith("{", StringComparison.Ordinal))
+                location = $"shell:::{location}";
+
+            location = location.Trim(' ', '/', '\\', '\n', '\'', '"');
+
+            return location.Replace('/', '\\');
         }
+
+        #endregion
     }
+
+    #region COM Interfaces
+
+    // Inspired by: https://github.com/w4po/ExplorerTabUtility
+
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    [Guid("6d5140c1-7436-11ce-8034-00aa006009fa")]
+    [ComImport]
+    public interface IServiceProvider
+    {
+        [PreserveSig]
+        int QueryService(ref Guid guidService, ref Guid riid, [MarshalAs(UnmanagedType.Interface)] out IShellBrowser ppvObject);
+    }
+
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    [Guid("000214E2-0000-0000-C000-000000000046")]
+    [ComImport]
+    public interface IShellBrowser
+    {
+        [PreserveSig]
+        int GetWindow(out nint handle);
+    }
+
+    #endregion
 }
