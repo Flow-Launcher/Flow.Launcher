34a58bc5da51831b2b6cc2e3396d1f073569961e
Jack251970 <1160210343@qq.com>
2025-09-05 18:39:31 +0800

    Add InverseBoolConverter

---
+++ Plugins/Flow.Launcher.Plugin.Url/Converters/InverseBoolConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Data;
+
+namespace Flow.Launcher.Plugin.Url.Converters;
+
+[ValueConversion(typeof(bool), typeof(bool))]
+public class InverseBoolConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+    {
+        if (targetType != typeof(bool))
+            throw new InvalidOperationException("The target must be a boolean");
+
+        return !(bool)value;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+    {
+        throw new NotSupportedException();
+    }
+}
