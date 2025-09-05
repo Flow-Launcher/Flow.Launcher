33aef7eb546bda7e7171087b0f342fbb3e8e9668
Jack251970 <1160210343@qq.com>
2025-09-05 19:02:11 +0800

    Add convert back implementation for InverseBoolConverter

--- Plugins/Flow.Launcher.Plugin.Url/Converters/InverseBoolConverter.cs
+++ Plugins/Flow.Launcher.Plugin.Url/Converters/InverseBoolConverter.cs
@@ -9,14 +9,17 @@ public class InverseBoolConverter : IValueConverter
 {
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
     {
-        if (targetType != typeof(bool))
-            throw new InvalidOperationException("The target must be a boolean");
+        if (value is not bool)
+            throw new ArgumentException("value should be boolean", nameof(value));
 
         return !(bool)value;
     }
 
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
     {
-        throw new NotSupportedException();
+        if (value is not bool)
+            throw new ArgumentException("value should be boolean", nameof(value));
+
+        return !(bool)value;
     }
 }
