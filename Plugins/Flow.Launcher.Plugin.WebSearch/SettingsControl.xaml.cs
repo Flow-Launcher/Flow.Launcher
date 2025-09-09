ff3688b22601454dac7558bd5717c8d3df8a793a
Jack251970 <1160210343@qq.com>
2025-09-09 16:42:46 +0800

    Add auto resize for list view in web search plugin

--- Plugins/Flow.Launcher.Plugin.WebSearch/SettingsControl.xaml.cs
+++ Plugins/Flow.Launcher.Plugin.WebSearch/SettingsControl.xaml.cs
@@ -140,5 +140,28 @@ namespace Flow.Launcher.Plugin.WebSearch
                 webSearch.ShowDialog();
             }
         }
+
+        private void ListView_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            var listView = sender as ListView;
+            var gView = listView.View as GridView;
+
+            var workingWidth =
+                listView.ActualWidth - SystemParameters.VerticalScrollBarWidth; // take into account vertical scrollbar
+
+            if (workingWidth <= 0) return;
+
+            var col1 = 0.08;
+            var col2 = 0.20;
+            var col3 = 0.30;
+            var col4 = 0.20;
+            var col5 = 0.22;
+
+            gView.Columns[0].Width = workingWidth * col1;
+            gView.Columns[1].Width = workingWidth * col2;
+            gView.Columns[2].Width = workingWidth * col3;
+            gView.Columns[3].Width = workingWidth * col4;
+            gView.Columns[4].Width = workingWidth * col5;
+        }
     }
 }
