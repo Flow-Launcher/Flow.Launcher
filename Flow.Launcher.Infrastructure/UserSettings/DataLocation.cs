99b7bdab27a2a05d695a75c3b992425fd08d5dec
Jack251970 <1160210343@qq.com>
2025-09-21 16:21:38 +0800

    Fix build issue

--- Flow.Launcher.Infrastructure/UserSettings/DataLocation.cs
+++ Flow.Launcher.Infrastructure/UserSettings/DataLocation.cs
@@ -1,16 +1,14 @@
 ﻿using System;
 using System.IO;
 
-#pragma warning disable CA2211 // Non-constant fields should not be visible
-
 namespace Flow.Launcher.Infrastructure.UserSettings
 {
     public static class DataLocation
     {
         public const string PortableFolderName = "UserData";
         public const string DeletionIndicatorFile = ".dead";
-        public static string PortableDataPath = Path.Combine(Constant.ProgramDirectory, PortableFolderName);
-        public static string RoamingDataPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "FlowLauncher");
+        public static readonly string PortableDataPath = Path.Combine(Constant.ProgramDirectory, PortableFolderName);
+        public static readonly string RoamingDataPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "FlowLauncher");
         public static string DataDirectory()
         {
             if (PortableDataLocationInUse())
@@ -22,27 +20,27 @@ namespace Flow.Launcher.Infrastructure.UserSettings
         public static bool PortableDataLocationInUse()
         {
             if (Directory.Exists(PortableDataPath) &&
-                !File.Exists(Path.Combine(PortableDataPath, DeletionIndicatorFile))
+                !File.Exists(Path.Combine(PortableDataPath, DeletionIndicatorFile)))
                 return true;
 
             return false;
         }
 
-        public static string VersionLogDirectory => Path.Combine(LogDirectory, Constant.Version);
-        public static string LogDirectory => Path.Combine(DataDirectory(), Constant.Logs);
+        public static readonly string VersionLogDirectory = Path.Combine(LogDirectory, Constant.Version);
+        public static readonly string LogDirectory = Path.Combine(DataDirectory(), Constant.Logs);
 
-        public static string CacheDirectory => Path.Combine(DataDirectory(), Constant.Cache);
-        public static string SettingsDirectory => Path.Combine(DataDirectory(), Constant.Settings);
-        public static string PluginsDirectory => Path.Combine(DataDirectory(), Constant.Plugins);
-        public static string ThemesDirectory => Path.Combine(DataDirectory(), Constant.Themes);
+        public static readonly string CacheDirectory = Path.Combine(DataDirectory(), Constant.Cache);
+        public static readonly string SettingsDirectory = Path.Combine(DataDirectory(), Constant.Settings);
+        public static readonly string PluginsDirectory = Path.Combine(DataDirectory(), Constant.Plugins);
+        public static readonly string ThemesDirectory = Path.Combine(DataDirectory(), Constant.Themes);
 
-        public static string PluginSettingsDirectory => Path.Combine(SettingsDirectory, Constant.Plugins);
-        public static string PluginCacheDirectory => Path.Combine(DataDirectory(), Constant.Cache, Constant.Plugins);
+        public static readonly string PluginSettingsDirectory = Path.Combine(SettingsDirectory, Constant.Plugins);
+        public static readonly string PluginCacheDirectory = Path.Combine(DataDirectory(), Constant.Cache, Constant.Plugins);
 
         public const string PythonEnvironmentName = "Python";
         public const string NodeEnvironmentName = "Node.js";
         public const string PluginEnvironments = "Environments";
         public const string PluginDeleteFile = "NeedDelete.txt";
-        public static string PluginEnvironmentsPath => Path.Combine(DataDirectory(), PluginEnvironments);
+        public static readonly string PluginEnvironmentsPath = Path.Combine(DataDirectory(), PluginEnvironments);
     }
 }
