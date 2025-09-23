6b5e20bc3baea47f9e1a120a0a9edcc44a1102e3
Jack251970 <1160210343@qq.com>
2025-09-23 11:20:57 +0800

    Improve code quality

--- Plugins/Flow.Launcher.Plugin.PluginIndicator/Main.cs
+++ Plugins/Flow.Launcher.Plugin.PluginIndicator/Main.cs
@@ -1,84 +1,83 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 
-namespace Flow.Launcher.Plugin.PluginIndicator
+namespace Flow.Launcher.Plugin.PluginIndicator;
+
+public class Main : IPlugin, IPluginI18n, IHomeQuery
 {
-    public class Main : IPlugin, IPluginI18n, IHomeQuery
+    internal static PluginInitContext Context { get; private set; }
+
+    public void Init(PluginInitContext context)
     {
-        internal static PluginInitContext Context { get; private set; }
+        Context = context;
+    }
 
-        public List<Result> Query(Query query)
-        {
-            return QueryResults(query);
-        }
+    public List<Result> Query(Query query)
+    {
+        return QueryResults(query);
+    }
 
-        public List<Result> HomeQuery()
-        {
-            return QueryResults();
-        }
+    private static List<Result> QueryResults(Query query = null)
+    {
+        var nonGlobalPlugins = GetNonGlobalPlugins();
+        var querySearch = query?.Search ?? string.Empty;
 
-        private List<Result> QueryResults(Query query = null)
-        {
-            var nonGlobalPlugins = GetNonGlobalPlugins();
-            var querySearch = query?.Search ?? string.Empty;
-
-            var results =
-                from keyword in nonGlobalPlugins.Keys
-                let plugin = nonGlobalPlugins[keyword].Metadata
-                let keywordSearchResult = Context.API.FuzzySearch(querySearch, keyword)
-                let searchResult = keywordSearchResult.IsSearchPrecisionScoreMet() ? keywordSearchResult : Context.API.FuzzySearch(querySearch, plugin.Name)
-                let score = searchResult.Score
-                where (searchResult.IsSearchPrecisionScoreMet()
-                        || string.IsNullOrEmpty(querySearch)) // To list all available action keywords
-                    && !plugin.Disabled
-                select new Result
-                {
-                    Title = keyword,
-                    SubTitle = Localize.flowlauncher_plugin_pluginindicator_result_subtitle(plugin.Name),
-                    Score = score,
-                    IcoPath = plugin.IcoPath,
-                    AutoCompleteText = $"{keyword}{Plugin.Query.TermSeparator}",
-                    Action = c =>
-                    {
-                        Context.API.ChangeQuery($"{keyword}{Plugin.Query.TermSeparator}");
-                        return false;
-                    }
-                };
-            return [.. results];
-        }
-
-        private Dictionary<string, PluginPair> GetNonGlobalPlugins()
-        {
-            var nonGlobalPlugins = new Dictionary<string, PluginPair>();
-            foreach (var plugin in Context.API.GetAllPlugins())
+        var results =
+            from keyword in nonGlobalPlugins.Keys
+            let plugin = nonGlobalPlugins[keyword].Metadata
+            let keywordSearchResult = Context.API.FuzzySearch(querySearch, keyword)
+            let searchResult = keywordSearchResult.IsSearchPrecisionScoreMet() ? keywordSearchResult : Context.API.FuzzySearch(querySearch, plugin.Name)
+            let score = searchResult.Score
+            where (searchResult.IsSearchPrecisionScoreMet()
+                    || string.IsNullOrEmpty(querySearch)) // To list all available action keywords
+                && !plugin.Disabled
+            select new Result
             {
-                foreach (var actionKeyword in plugin.Metadata.ActionKeywords)
+                Title = keyword,
+                SubTitle = Localize.flowlauncher_plugin_pluginindicator_result_subtitle(plugin.Name),
+                Score = score,
+                IcoPath = plugin.IcoPath,
+                AutoCompleteText = $"{keyword}{Plugin.Query.TermSeparator}",
+                Action = c =>
                 {
-                    // Skip global keywords
-                    if (actionKeyword == Plugin.Query.GlobalPluginWildcardSign) continue;
-
-                    // Skip dulpicated keywords
-                    if (nonGlobalPlugins.ContainsKey(actionKeyword)) continue;
-
-                    nonGlobalPlugins.Add(actionKeyword, plugin);
+                    Context.API.ChangeQuery($"{keyword}{Plugin.Query.TermSeparator}");
+                    return false;
                 }
+            };
+        return [.. results];
+    }
+
+    private static Dictionary<string, PluginPair> GetNonGlobalPlugins()
+    {
+        var nonGlobalPlugins = new Dictionary<string, PluginPair>();
+        foreach (var plugin in Context.API.GetAllPlugins())
+        {
+            foreach (var actionKeyword in plugin.Metadata.ActionKeywords)
+            {
+                // Skip global keywords
+                if (actionKeyword == Plugin.Query.GlobalPluginWildcardSign) continue;
+
+                // Skip dulpicated keywords
+                if (nonGlobalPlugins.ContainsKey(actionKeyword)) continue;
+
+                nonGlobalPlugins.Add(actionKeyword, plugin);
             }
-            return nonGlobalPlugins;
         }
+        return nonGlobalPlugins;
+    }
 
-        public void Init(PluginInitContext context)
-        {
-            Context = context;
-        }
+    public string GetTranslatedPluginTitle()
+    {
+        return Localize.flowlauncher_plugin_pluginindicator_plugin_name();
+    }
 
-        public string GetTranslatedPluginTitle()
-        {
-            return Localize.flowlauncher_plugin_pluginindicator_plugin_name();
-        }
+    public string GetTranslatedPluginDescription()
+    {
+        return Localize.flowlauncher_plugin_pluginindicator_plugin_description();
+    }
 
-        public string GetTranslatedPluginDescription()
-        {
-            return Localize.flowlauncher_plugin_pluginindicator_plugin_description();
-        }
+    public List<Result> HomeQuery()
+    {
+        return QueryResults();
     }
 }
