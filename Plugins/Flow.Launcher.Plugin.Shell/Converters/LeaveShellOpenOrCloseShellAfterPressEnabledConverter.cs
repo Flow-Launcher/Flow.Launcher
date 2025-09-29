d106c5144b7a5397408d566ca5acf2faf1606ae0
Jack251970 <1160210343@qq.com>
2025-09-29 23:12:02 +0800

    Fix IsEnabled logic

--- Plugins/Flow.Launcher.Plugin.Shell/Converters/LeaveShellOpenOrCloseShellAfterPressEnabledConverter.cs
+++ Plugins/Flow.Launcher.Plugin.Shell/Converters/LeaveShellOpenOrCloseShellAfterPressEnabledConverter.cs
@@ -4,18 +4,18 @@ using System.Windows.Data;
 
 namespace Flow.Launcher.Plugin.Shell.Converters;
 
-public class LeaveShellOpenEnabledConverter : IMultiValueConverter
+public class LeaveShellOpenOrCloseShellAfterPressEnabledConverter : IMultiValueConverter
 {
     public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
     {
         if (
             values.Length != 2 ||
-            values[0] is not bool closeShellAfterPress ||
+            values[0] is not bool closeShellAfterPressOrLeaveShellOpen ||
             values[1] is not Shell shell
         )
             return Binding.DoNothing;
 
-        return (!closeShellAfterPress) && shell != Shell.RunCommand;
+        return (!closeShellAfterPressOrLeaveShellOpen) && shell != Shell.RunCommand;
     }
 
     public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
