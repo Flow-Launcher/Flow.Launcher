9d7ac851bd1c17f121da9218780d678b140c0fbd
Jack251970 <1160210343@qq.com>
2025-10-03 16:04:26 +0800

    Show setting panel for initializing or init failed plugins

--- Flow.Launcher/ViewModel/PluginViewModel.cs
+++ Flow.Launcher/ViewModel/PluginViewModel.cs
@@ -132,7 +132,6 @@ namespace Flow.Launcher.ViewModel
         public Control BottomPart2 => IsExpanded ? _bottomPart2 ??= new InstalledPluginDisplayBottomData() : null;
 
         public bool HasSettingControl =>
-            !PluginManager.IsInitializingOrInitFailed(PluginPair.Metadata.ID) && // Do not show setting panel for initializing or init failed plugins
             PluginPair.Plugin is ISettingProvider &&
             (PluginPair.Plugin is not JsonRPCPluginBase jsonRPCPluginBase || // Is not JsonRPC plugin
             jsonRPCPluginBase.NeedCreateSettingPanel()); // Is JsonRPC plugin and need to create setting panel
