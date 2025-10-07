e5736567f67353de6552ece5accd5e6b9c3621db
01Dri <diegomagalhaesdev@gmail.com>
2025-10-07 00:33:16 -0300

    feat: created a base History model

---
+++ Flow.Launcher/Storage/History.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Flow.Launcher.Infrastructure.UserSettings;
+using Flow.Launcher.Plugin;
+using JetBrains.Annotations;
+
+namespace Flow.Launcher.Storage
+{
+    public class History
+    {
+        [JsonInclude] 
+        public List<HistoryItem> LastOpenedHistoryItems { get; private set; } = [];
+        [JsonInclude] 
+        public List<HistoryItem> QueryHistoryItems { get; private set; } = [];
+
+        private int _maxHistory = 300;
+
+        public void AddToHistory(Result result, Settings settings)
+        {
+            if (settings.ShowHistoryQueryResultsForHomePage)
+            {
+                AddLastQuery(result);
+                return;
+            }
+            AddLastOpened(result);
+        }
+
+        public List<HistoryItem> GetHistoryItems(Settings settings)
+        { 
+            if (settings.ShowHistoryQueryResultsForHomePage) return QueryHistoryItems;
+            if (settings.ShowHistoryLastOpenedResultsForHomePage) return LastOpenedHistoryItems;
+            return new List<HistoryItem>();
+        }
+
+        private void AddLastQuery(Result result)
+        {
+            if (string.IsNullOrEmpty(result.OriginQuery.RawQuery)) return;
+            if (QueryHistoryItems.Count > _maxHistory)
+            {
+                QueryHistoryItems.RemoveAt(0);
+            }
+
+            if (QueryHistoryItems.Count > 0 && QueryHistoryItems.Last().OriginQuery == result.OriginQuery)
+            {
+                QueryHistoryItems.Last().ExecutedDateTime = DateTime.Now;
+            }
+            else
+            {
+                QueryHistoryItems.Add(new HistoryItem
+                {
+                    OriginQuery = result.OriginQuery,
+                    ExecutedDateTime = DateTime.Now
+                });
+            }
+        }
+
+        private void AddLastOpened(Result result)
+        {
+            var item = new HistoryItem
+            {
+                Title = result.Title,
+                SubTitle = result.SubTitle,
+                IcoPath = result.IcoPath ?? string.Empty,
+                PluginID = result.PluginID,
+                OriginQuery = result.OriginQuery,
+                ExecutedDateTime = DateTime.Now,
+            };
+
+            var existing = LastOpenedHistoryItems.
+                FirstOrDefault(x => x.Title == item.Title && x.PluginID == item.PluginID);
+
+
+            if (existing != null)
+            {
+                existing.ExecutedDateTime = DateTime.Now;
+            }
+            else
+            {
+                if (LastOpenedHistoryItems.Count > _maxHistory)
+                {
+                    LastOpenedHistoryItems.RemoveAt(0);
+                }
+
+                LastOpenedHistoryItems.Add(item);
+            }
+        }
+    }
+}
