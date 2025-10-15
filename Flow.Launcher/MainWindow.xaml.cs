c73689fbc3701812d0e05a47c4add7fad959a044
Jack251970 <1160210343@qq.com>
2025-10-15 12:24:36 +0800

    Fix spelling

--- Flow.Launcher/MainWindow.xaml.cs
+++ Flow.Launcher/MainWindow.xaml.cs
@@ -860,7 +860,7 @@ namespace Flow.Launcher
 
         public void UpdatePosition()
         {
-            // Initialize call twice to work around multi-display alignment issue- https://github.com/Flow-Launcher/Flow.Launcher/issues/2910
+            // Initialize call twice to workaround multi-display alignment issue- https://github.com/Flow-Launcher/Flow.Launcher/issues/2910
             if (_viewModel.IsDialogJumpWindowUnderDialog())
             {
                 InitializeDialogJumpPosition();
@@ -884,7 +884,7 @@ namespace Flow.Launcher
 
         private void InitializePosition()
         {
-            // Initialize call twice to work around multi-display alignment issue- https://github.com/Flow-Launcher/Flow.Launcher/issues/2910
+            // Initialize call twice to workaround multi-display alignment issue- https://github.com/Flow-Launcher/Flow.Launcher/issues/2910
             InitializePositionInner();
             InitializePositionInner();
             return;
