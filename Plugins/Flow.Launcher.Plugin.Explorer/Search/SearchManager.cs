bb490c0ceff074f5128227be3c33ef5725d590da
Jeremy Wu <jeremy24wu@gmail.com>
2025-12-06 19:25:49 +1100

    formatting

--- Plugins/Flow.Launcher.Plugin.Explorer/Search/SearchManager.cs
+++ Plugins/Flow.Launcher.Plugin.Explorer/Search/SearchManager.cs
@@ -58,6 +58,7 @@ namespace Flow.Launcher.Plugin.Explorer.Search
             var results = new HashSet<Result>(PathEqualityComparator.Instance);
 
             var keyword = query.ActionKeyword.Length == 0 ? Query.GlobalPluginWildcardSign : query.ActionKeyword;
+
             // No action keyword matched - plugin should not handle this query, return empty results.
             var activeActionKeywords = Settings.GetActiveActionKeywords(keyword);
             if (activeActionKeywords.Count == 0)
