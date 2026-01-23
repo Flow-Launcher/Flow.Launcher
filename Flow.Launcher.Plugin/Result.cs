0f0ee9ffde013b3598b8ea83f304a9b5701ccc45
Jack251970 <1160210343@qq.com>
2026-01-23 16:05:03 +0800

    Revert "Try to fix combability issue"
    This reverts commit b703bcdd52c72103b7dbe794fe171dfd8a8c08f7.

--- Flow.Launcher.Plugin/Result.cs
+++ Flow.Launcher.Plugin/Result.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
@@ -164,16 +164,10 @@ namespace Flow.Launcher.Plugin
         [JsonIgnore]
         public IconDelegate BadgeIcon = null;
 
-        private GlyphInfo _glyph;
-        
         /// <summary>
         /// Information for Glyph Icon (Prioritized than IcoPath/Icon if user enable Glyph Icons)
         /// </summary>
-        public GlyphInfo Glyph
-        {
-            get => _glyph;
-            set => _glyph = value;
-        }
+        public GlyphInfo Glyph { get; set; }
 
         /// <summary>
         /// An action to take in the form of a function call when the result has been selected.
