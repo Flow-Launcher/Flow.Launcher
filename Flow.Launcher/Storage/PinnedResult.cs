0076308fa4b542f3bc1c5af1126c78ed58073b85
dridev <diegomagalhaesdev@gmail.com>
2026-03-08 13:13:01 -0300

    Pinned result object and storage

---
+++ Flow.Launcher/Storage/PinnedResult.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Flow.Launcher.Storage
+{
+    public class PinnedResult
+    {
+        public List<PinnedResultItem> Item { set; get; } = [];
+    }
+}
---
+++ Flow.Launcher/Storage/PinnedResultItem.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Flow.Launcher.Plugin;
+
+namespace Flow.Launcher.Storage
+{
+    public class PinnedResultItem : Result
+    {
+        public DateTime AddAt { get; set; }
+    }
+}
