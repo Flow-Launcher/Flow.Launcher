3b22ef863c37ee5fe39936468c4db8d5c5a409c4
Jack251970 <1160210343@qq.com>
2026-03-14 23:30:59 +0800

    Revert "Boost home folder score only if score > 0"
    This reverts commit d0b60bde5f2d9400fe51274c929ca35faf340375.

--- Plugins/Flow.Launcher.Plugin.Explorer/Search/ResultManager.cs
+++ Plugins/Flow.Launcher.Plugin.Explorer/Search/ResultManager.cs
@@ -130,7 +130,7 @@ namespace Flow.Launcher.Plugin.Explorer.Search
 
         internal static Result CreateFolderResult(string title, string subtitle, string path, Query query, int score = 0, bool windowsIndexed = false)
         {
-            if (IsHomeFolderPath(path) && score > 0)
+            if (IsHomeFolderPath(path))
                 score += HomeFolderScoreBoost;
 
             return new Result
