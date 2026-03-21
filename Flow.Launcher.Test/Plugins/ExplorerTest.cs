18dff57092b6980dbb7adb747ea481fd697977e8
Jack Ye <jack1160210343@gmail.com>
2026-03-21 14:01:47 +0800

    Update test
    Co-authored-by: Jeremy Wu <jeremy24wu@gmail.com>

--- Flow.Launcher.Test/Plugins/ExplorerTest.cs
+++ Flow.Launcher.Test/Plugins/ExplorerTest.cs
@@ -473,7 +473,7 @@ namespace Flow.Launcher.Test.Plugins
                 Path.Combine(desktopPath, "dummy_desktop_file"),
                 Path.Combine(desktopPath, "dummy_desktop_folder") + "\\\\",
                 desktopPath + "\\\\dummy_desktop_folder\\\\",
-                Path.Combine(desktopPath, "dummy_desktop_file", "dummy_file"),
+                Path.Combine(desktopPath, "dummy_desktop_folder", "dummy_file"),
             };
 
             foreach (var path in homeFolderVariants)
