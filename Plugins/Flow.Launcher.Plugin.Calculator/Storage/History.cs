283cbe4555e1d8e794600ba4564fc03f1e95e9f3
dridev <diegomagalhaesdev@gmail.com>
2026-05-02 16:41:35 -0300

    first implementation of history

---
+++ Plugins/Flow.Launcher.Plugin.Calculator/Storage/History.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Flow.Launcher.Plugin.Calculator.Storage;
+
+internal class History
+{
+    public List<HistoryItem> Items { get; set; } = [];
+
+    public void AddOrUpdate(Result result, Func<ActionContext, bool> action)
+    {
+        var item = new HistoryItem(result, action);
+        Items.Add(item);
+    }
+}
---
+++ Plugins/Flow.Launcher.Plugin.Calculator/Storage/HistoryItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Flow.Launcher.Plugin.Calculator.Storage;
+
+internal class HistoryItem : Result
+{
+    public string Query { get; set; }
+    public DateTime CalculatedAt { get; set; }
+
+    public HistoryItem(Result result, Func<ActionContext, bool> action)
+    {
+        Title = result.Title;
+        SubTitle = result.SubTitle;
+        //PluginID = result.PluginID;
+        //Query = result.OriginQuery.TrimmedQuery;
+        //OriginQuery = result.OriginQuery;
+        RecordKey = result.RecordKey;
+        IcoPath = result.IcoPath;
+        PluginDirectory = result.PluginDirectory;
+        Glyph = result.Glyph;
+        //Query = result.Ori
+        Query = "1+2";
+        Action = action;
+    }
+}
