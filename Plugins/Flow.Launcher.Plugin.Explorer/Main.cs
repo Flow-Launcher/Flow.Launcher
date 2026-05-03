d267f3fd8c09dd650fea51c87ae0099b7b479c1c
VictoriousRaptor <10308169+VictoriousRaptor@users.noreply.github.com>
2026-05-03 14:20:57 +0800

    Remove x86 Everything dll

--- Plugins/Flow.Launcher.Plugin.Explorer/Main.cs
+++ Plugins/Flow.Launcher.Plugin.Explorer/Main.cs
@@ -46,8 +46,7 @@ namespace Flow.Launcher.Plugin.Explorer
             searchManager = new SearchManager(Settings, Context);
             ResultManager.Init(Context, Settings);
 
-            var sdkDirectory = Path.Combine(Context.CurrentPluginMetadata.PluginDirectory, "EverythingSDK",
-                Environment.Is64BitProcess ? "x64" : "x86");
+            var sdkDirectory = Path.Combine(Context.CurrentPluginMetadata.PluginDirectory, "EverythingSDK", "x64");
             Settings.EverythingManagerInstance.InitializeApi(sdkDirectory);
             return Task.CompletedTask;
         }
